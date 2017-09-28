00499fa0d7bccca0469d5910b9ccc1156fa501cc
gcarq <michael.egger@tsn.at>
2017-09-28 23:26:56 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from freqtrade import __version__
+
+
+setup(name='freqtrade',
+      version=__version__,
+      description='Simple High Frequency Trading Bot for crypto currencies',
+      url='https://github.com/gcarq/freqtrade',
+      author='gcarq and contributors',
+      author_email='michael.egger@tsn.at',
+      license='GPLv3',
+      packages=['freqtrade'],
+      zip_safe=False)
