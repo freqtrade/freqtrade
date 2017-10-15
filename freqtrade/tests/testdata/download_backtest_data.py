389f9b45bb49d9f9fe69ed6d4e0ffbcec207135b
dertione <mormotrobien@gmail.com>
2017-10-15 17:24:49 +0200

    update pylint 10/10

--- freqtrade/tests/testdata/download_backtest_data.py
+++ freqtrade/tests/testdata/download_backtest_data.py
@@ -1,16 +1,16 @@
-"""This module download automatically json data of crypto currencies in bittrex"""
 #!/usr/bin/env python3
+
+"""This script generate json data from bittrex"""
 
 from urllib.request import urlopen
 
 CURRENCIES = ["ok", "neo", "dash", "etc", "eth", "snt"]
 
 for cur in CURRENCIES:
-	url1 = 'https://bittrex.com/Api/v2.0/pub/market/GetTicks?marketName=BTC-'
-	url2 = '&tickInterval=fiveMin'
-	url = url1+cur+url2
-	x = urlopen(url)
-	json_data = x.read()
-	json_str = str(json_data, 'utf-8')
-	with open("btc-"+cur+".json", "w") as file:
-		file.write(json_str)
+    url1 = 'https://bittrex.com/Api/v2.0/pub/market/GetTicks?marketName=BTC-'
+    url = url1+cur+'&tickInterval=fiveMin'
+    x = urlopen(url)
+    json_data = x.read()
+    json_str = str(json_data, 'utf-8')
+    with open('btc-'+cur+'.json', 'w') as file:
+        file.write(json_str)
