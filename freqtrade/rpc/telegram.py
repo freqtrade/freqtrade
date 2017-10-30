26468bef83d2851f9e99182a7f6b789a6d1ef846
gcarq <michael.egger@tsn.at>
2017-10-31 00:29:22 +0100

    balance: filter currencies with 0.0 balances

--- freqtrade/rpc/telegram.py
+++ freqtrade/rpc/telegram.py
@@ -205,14 +205,14 @@ def _profit(bot: Bot, update: Update) -> None:
 @authorized_only
 def _balance(bot: Bot, update: Update) -> None:
     """
-    Hander for /balance
+    Handler for /balance
     Returns current account balance per crypto
     """
     output = ""
-
     balances = exchange.get_balances()
-
     for currency in balances:
+        if not currency['Balance'] and not currency['Available'] and not currency['Pending']:
+            continue
         output += """*Currency*: {Currency}
 *Available*: {Available}
 *Balance*: {Balance}
