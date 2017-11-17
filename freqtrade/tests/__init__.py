8655c6c26490498defd0bbfbfa837c95695eae37
gcarq <michael.egger@tsn.at>
2017-11-17 17:32:34 +0100

    reduce backtest data samples to 10

--- freqtrade/tests/__init__.py
+++ freqtrade/tests/__init__.py
@@ -6,10 +6,8 @@ def load_backtesting_data(ticker_interval: int = 5):
     path = os.path.abspath(os.path.dirname(__file__))
     result = {}
     pairs = [
-        'BTC_BCC', 'BTC_ETH', 'BTC_MER', 'BTC_POWR', 'BTC_ETC',
-        'BTC_OK', 'BTC_NEO', 'BTC_EMC2', 'BTC_DASH', 'BTC_LSK',
-        'BTC_LTC', 'BTC_XZC', 'BTC_OMG', 'BTC_STRAT', 'BTC_XRP',
-        'BTC_QTUM', 'BTC_WAVES', 'BTC_VTC', 'BTC_XLM', 'BTC_MCO'
+        'BTC_BCC', 'BTC_ETH', 'BTC_DASH', 'BTC_POWR', 'BTC_ETC',
+        'BTC_VTC', 'BTC_WAVES', 'BTC_LSK', 'BTC_XLM', 'BTC_OK',
     ]
     for pair in pairs:
         with open('{abspath}/testdata/{pair}-{ticker_interval}.json'.format(
