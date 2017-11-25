2fe11cd77ac45e3291405ab09dc6e289d2a493b5
gcarq <michael.egger@tsn.at>
2017-11-25 03:28:18 +0100

    add helper scripts for mongodb

--- scripts/start-hyperopt-worker.sh
+++ scripts/start-hyperopt-worker.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
 DB_NAME=freqtrade_hyperopt
 
-hyperopt-mongo-worker --mongo=127.0.0.1:1234/${DB_NAME} --poll-interval=0.1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORK_DIR="${DIR}/../.hyperopt/worker"
+
+mkdir -p "${WORK_DIR}"
+hyperopt-mongo-worker \
+    --mongo="127.0.0.1:1234/${DB_NAME}" \
+    --poll-interval=0.1 \
+    --workdir="${WORK_DIR}"
--- scripts/start-mongodb.sh
+++ scripts/start-mongodb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DB_PATH="${DIR}/../.mongodb"
+DB_PATH="${DIR}/../.hyperopt/mongodb"
 
-mkdir -p ${DB_PATH}
-mongod --dbpath ${DB_PATH} \
+mkdir -p "${DB_PATH}"
+mongod --dbpath "${DB_PATH}" \
     --bind_ip 127.0.0.1 \
     --port 1234 \
     --directoryperdb \
