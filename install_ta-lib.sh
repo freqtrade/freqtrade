98650acca0a2a0814ffd90f738a387ad340ca884
Samuel Husso <shusso@iki.fi>
2017-12-18 10:26:48 +0200

    use curl instead of wget (see travis-ci/issues/5059)

--- install_ta-lib.sh
+++ install_ta-lib.sh
@@ -1,5 +1,5 @@
 if [ ! -f "ta-lib/CHANGELOG.TXT" ]; then
-  wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+  curl -O http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
   tar zxvf ta-lib-0.4.0-src.tar.gz
   cd ta-lib && ./configure && make && sudo make install && cd ..
 else
