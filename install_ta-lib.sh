92f6db5bd7dd7233af547d512dbbe45dc4536076
Janne Sinivirta <janne.sinivirta@gmail.com>
2017-12-18 09:36:29 +0200

    fix checking for cached ta-lib

--- install_ta-lib.sh
+++ install_ta-lib.sh
@@ -1,4 +1,4 @@
-if [ ! -d "ta-lib" ]; then
+if [ ! -f "ta-lib/CHANGELOG.TXT" ]; then
   wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
   tar zxvf ta-lib-0.4.0-src.tar.gz
   cd ta-lib && ./configure && make && sudo make install && cd ..
