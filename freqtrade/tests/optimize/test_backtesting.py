37613fc056705e3fa6435a630cf922bac4fa7a67
kryofly <kryofly@space>
2017-12-29 17:53:58 +0100

    flake8

--- freqtrade/tests/optimize/test_backtesting.py
+++ freqtrade/tests/optimize/test_backtesting.py
@@ -1,7 +1,6 @@
 # pragma pylint: disable=missing-docstring,W0212
 
 import math
-import os
 import pandas as pd
 from freqtrade import exchange, optimize
 from freqtrade.exchange import Bittrex
@@ -48,78 +47,6 @@ def test_backtest_1min_ticker_interval(default_conf, mocker):
     data = optimize.load_data(ticker_interval=1, pairs=['BTC_UNITEST'])
     results = backtest(default_conf['stake_amount'], optimize.preprocess(data), 1, True)
     assert not results.empty
-
-
-def test_backtest_with_new_pair(default_conf, ticker_history, mocker):
-    mocker.patch('freqtrade.optimize.get_ticker_history', return_value=ticker_history)
-    mocker.patch.dict('freqtrade.main._CONF', default_conf)
-
-    exchange._API = Bittrex({'key': '', 'secret': ''})
-
-    optimize.load_data(ticker_interval=1, pairs=['BTC_MEME'])
-    file = 'freqtrade/tests/testdata/BTC_MEME-1.json'
-    assert os.path.isfile(file) is True
-
-    # delete file freshly downloaded
-    if os.path.isfile(file):
-        os.remove(file)
-
-
-def test_testdata_path():
-    assert os.path.join('freqtrade', 'tests', 'testdata') in testdata_path()
-
-
-def test_download_pairs(default_conf, ticker_history, mocker):
-    mocker.patch('freqtrade.optimize.__init__.get_ticker_history', return_value=ticker_history)
-    mocker.patch.dict('freqtrade.main._CONF', default_conf)
-    exchange._API = Bittrex({'key': '', 'secret': ''})
-
-    file1_1 = 'freqtrade/tests/testdata/BTC_MEME-1.json'
-    file1_5 = 'freqtrade/tests/testdata/BTC_MEME-5.json'
-    file2_1 = 'freqtrade/tests/testdata/BTC_CFI-1.json'
-    file2_5 = 'freqtrade/tests/testdata/BTC_CFI-5.json'
-
-    assert download_pairs(pairs=['BTC-MEME', 'BTC-CFI']) is True
-
-    assert os.path.isfile(file1_1) is True
-    assert os.path.isfile(file1_5) is True
-    assert os.path.isfile(file2_1) is True
-    assert os.path.isfile(file2_5) is True
-
-    # delete files freshly downloaded
-    if os.path.isfile(file1_1):
-        os.remove(file1_1)
-
-    if os.path.isfile(file1_5):
-        os.remove(file1_5)
-
-    if os.path.isfile(file2_1):
-        os.remove(file2_1)
-
-    if os.path.isfile(file2_5):
-        os.remove(file2_5)
-
-
-def test_download_backtesting_testdata(default_conf, ticker_history, mocker):
-    mocker.patch('freqtrade.optimize.__init__.get_ticker_history', return_value=ticker_history)
-    mocker.patch.dict('freqtrade.main._CONF', default_conf)
-    exchange._API = Bittrex({'key': '', 'secret': ''})
-
-    # Download a 1 min ticker file
-    file1 = 'freqtrade/tests/testdata/BTC_XEL-1.json'
-    download_backtesting_testdata(pair="BTC-XEL", interval=1)
-    assert os.path.isfile(file1) is True
-
-    if os.path.isfile(file1):
-        os.remove(file1)
-
-    # Download a 5 min ticker file
-    file2 = 'freqtrade/tests/testdata/BTC_STORJ-5.json'
-    download_backtesting_testdata(pair="BTC-STORJ", interval=5)
-    assert os.path.isfile(file2) is True
-
-    if os.path.isfile(file2):
-        os.remove(file2)
 
 
 def trim_dataframe(df, num):
