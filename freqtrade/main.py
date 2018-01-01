b0b7efb81cb2dbb17dd730fe408ed51b439fbf1d
Samuel Husso <shusso@iki.fi>
2018-01-01 12:43:55 +0200

    Revert "Make get_signals async. This should speed up create_trade calls by at least 10x. (#223)"
    This reverts commit 67686583007e0a8260d84f525e3a76eef665dcfd.
    See details in #PR266

--- freqtrade/main.py
+++ freqtrade/main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-import asyncio
 import copy
 import json
 import logging
 import sys
 import time
 import traceback
-from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from typing import Dict, Optional, List
 
@@ -243,17 +241,8 @@ def create_trade(stake_amount: float) -> bool:
         raise DependencyException('No pair in whitelist')
 
     # Pick pair based on StochRSI buy signals
-    with ThreadPoolExecutor() as pool:
-        awaitable_signals = [
-            asyncio.wrap_future(pool.submit(get_signal, pair, SignalType.BUY))
-            for pair in whitelist
-        ]
-
-    loop = asyncio.get_event_loop()
-    signals = loop.run_until_complete(asyncio.gather(*awaitable_signals))
-
-    for idx, _pair in enumerate(whitelist):
-        if signals[idx]:
+    for _pair in whitelist:
+        if get_signal(_pair, SignalType.BUY):
             pair = _pair
             break
     else:
