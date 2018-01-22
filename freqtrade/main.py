757a46ab12761e7519cb172b495cc3aa2c62e754
Samuel Husso <shusso@iki.fi>
2018-01-22 10:39:26 +0200

    ticker_interval as int (instead of string)

--- freqtrade/main.py
+++ freqtrade/main.py
@@ -476,7 +476,7 @@ def main(sysargv=sys.argv[1:]) -> None:
                     _process,
                     min_secs=_CONF['internals'].get('process_throttle_secs', 10),
                     nb_assets=args.dynamic_whitelist,
-                    interval=int(_CONF.get('ticker_interval', "5"))
+                    interval=int(_CONF.get('ticker_interval', 5))
                 )
             old_state = new_state
     except KeyboardInterrupt:
