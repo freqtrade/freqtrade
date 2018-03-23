9fde3556a4605333b7b961432b662d4aa432f109
MoonGem <34537029+MoonGem@users.noreply.github.com>
2018-03-23 17:34:31 -0500

    Removed profit roi updated with config.json

--- user_data/hyperopt_conf.py
+++ user_data/hyperopt_conf.py
@@ -11,31 +11,52 @@ def hyperopt_optimize_conf() -> dict:
     :return:
     """
     return {
-        'max_open_trades': 3,
+        'max_open_trades': 15,
         'stake_currency': 'BTC',
-        'stake_amount': 0.01,
-        "minimal_roi": {
-            '40': 0.0,
-            '30': 0.01,
-            '20': 0.02,
-            '0': 0.04,
-        },
-        'stoploss': -0.10,
+        'stake_amount': 0.00075,
+        'ticker_interval': 5,
         "bid_strategy": {
             "ask_last_balance": 0.0
         },
         "exchange": {
             "pair_whitelist": [
-                "BTC_ETH",
-                "BTC_LTC",
-                "BTC_ETC",
-                "BTC_DASH",
-                "BTC_ZEC",
-                "BTC_XLM",
-                "BTC_NXT",
-                "BTC_POWR",
-                "BTC_ADA",
-                "BTC_XMR"
+            'BTC_ETH',
+            'BTC_XRP',
+            'BTC_BCC',
+            'BTC_LTC',
+            'BTC_ADA',
+            'BTC_XMR',
+            'BTC_DASH',
+            'BTC_TRX',
+            'BTC_ETC',
+            'BTC_ZEC',
+            'BTC_WAVES',
+            'BTC_STEEM',
+            'BTC_STRAT',
+            'BTC_DCR',
+            'BTC_REP',
+            'BTC_SNT',
+            'BTC_KMD',
+            'BTC_ARK',
+            'BTC_ARDR',
+            'BTC_MONA',
+            'BTC_DGB',
+            'BTC_PIVX',
+            'BTC_SYS',
+            'BTC_FCT',
+            'BTC_BAT',
+            'BTC_GNT',
+            'BTC_XZC',
+            'BTC_EMC',
+            'BTC_NXT',
+            'BTC_SALT',
+            'BTC_PAY',
+            'BTC_PART',
+            'BTC_GBYTE',
+            'BTC_BNT',
+            'BTC_POWR',
+            'BTC_NXS',
+            'BTC_SRN'
             ]
         }
     }
