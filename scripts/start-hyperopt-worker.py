3efcaaee59b2b7b786153619396129228f5dcff1
MoonGem <34537029+MoonGem@users.noreply.github.com>
2018-03-23 17:59:28 -0500

    Update start-hyperopt-worker.py

--- scripts/start-hyperopt-worker.py
+++ scripts/start-hyperopt-worker.py
@@ -19,6 +19,8 @@ command = [
     '--mongo=127.0.0.1:1234/{}'.format(DB_NAME),
     '--poll-interval=0.1',
     '--workdir={}'.format(WORK_DIR),
+    '--max-jobs=100000',
+    '--max-consecutive-failures=100000',
 ]
 processes = [subprocess.Popen(command) for i in range(PROC_COUNT)]
 
