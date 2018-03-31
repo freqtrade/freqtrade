2f40e23dcc870dbbe309f606c731bd88694b5d56
Matthias Voppichler <xmatthias@outlook.com>
2018-03-31 17:28:54 +0200

    don't check negated if both trees are handled

--- freqtrade/misc.py
+++ freqtrade/misc.py
@@ -71,13 +71,13 @@ def file_dump_json(filename, data, is_zip=False) -> None:
     :param data: JSON Data to save
     :return:
     """
-    if not is_zip:
-        with open(filename, 'w') as fp:
-            json.dump(data, fp, default=str)
-    else:
+    if is_zip:
         if not filename.endswith('.gz'):
             filename = filename + '.gz'
         with gzip.open(filename, 'w') as fp:
+            json.dump(data, fp, default=str)
+    else:
+        with open(filename, 'w') as fp:
             json.dump(data, fp, default=str)
 
 
