b199805c5d722921659be30f0f444c9c11d636b1
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-05-12 08:50:33 -0700

    wrote doc and started writing schemas

---
+++ freqtrade/aws/strategy.py
@@ -0,0 +1,39 @@
+
+def names(event, context):
+    """
+            returns the names of all registered strategies, but public and private
+    :param event:
+    :param context:
+    :return:
+    """
+    pass
+
+
+def performance(event, context):
+    """
+        returns the performance of the specified strategy
+    :param event:
+    :param context:
+    :return:
+    """
+    pass
+
+
+def code(event, context):
+    """
+        returns the code of the requested strategy, if it's public
+    :param event:
+    :param context:
+    :return:
+    """
+    pass
+
+
+def submit(event, context):
+    """
+        combiles the given strategy and stores it in the internal database
+    :param event:
+    :param context:
+    :return:
+    """
+    pass
