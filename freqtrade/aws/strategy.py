0c4b77610105f82687b46766389214ef2e87d0ad
Gert Wohlgemuth <berlinguyinca@gmail.com>
2018-05-17 08:02:51 -0700

    working on static library support

--- freqtrade/aws/strategy.py
+++ freqtrade/aws/strategy.py
@@ -1,3 +1,15 @@
+import os
+import ctypes
+
+for d, dirs, files in os.walk('lib'):
+    for f in files:
+        if f.endswith('.a'):
+            continue
+        ctypes.cdll.LoadLibrary(os.path.join(d, f))
+
+
+
+
 from freqtrade.strategy.resolver import StrategyResolver
 
 import simplejson as json
