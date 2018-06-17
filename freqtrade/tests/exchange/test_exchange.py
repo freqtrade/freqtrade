1e3d722bc2998da849f20a2fe84c9e9fe26c8371
xmatthias <xmatthias@outlook.com>
2018-06-17 23:22:28 +0200

    add test for get_trades

--- freqtrade/tests/exchange/test_exchange.py
+++ freqtrade/tests/exchange/test_exchange.py
@@ -3,6 +3,7 @@
 import logging
 from copy import deepcopy
 from random import randint
+from datetime import datetime
 from unittest.mock import MagicMock, PropertyMock
 
 import ccxt
@@ -567,6 +568,54 @@ def test_get_pair_detail_url(default_conf, mocker):
     url = exchange.get_pair_detail_url('LOOONG/BTC')
     assert 'LOOONG' in url
     assert 'BTC' in url
+
+
+def test_get_trades_for_order(default_conf, mocker):
+    order_id = 'ABCD-ABCD'
+    since = datetime(2018, 5, 5)
+    default_conf["dry_run"] = False
+    mocker.patch('freqtrade.exchange.Exchange.exchange_has', return_value=True)
+    api_mock = MagicMock()
+
+    api_mock.fetch_my_trades = MagicMock(return_value=[{'id': 'TTR67E-3PFBD-76IISV',
+                                                        'order': 'ABCD-ABCD',
+                                                        'info': {'pair': 'XLTCZBTC',
+                                                                 'time': 1519860024.4388,
+                                                                 'type': 'buy',
+                                                                 'ordertype': 'limit',
+                                                                 'price': '20.00000',
+                                                                 'cost': '38.62000',
+                                                                 'fee': '0.06179',
+                                                                 'vol': '5',
+                                                                 'id': 'ABCD-ABCD'},
+                                                        'timestamp': 1519860024438,
+                                                        'datetime': '2018-02-28T23:20:24.438Z',
+                                                        'symbol': 'LTC/BTC',
+                                                        'type': 'limit',
+                                                        'side': 'buy',
+                                                        'price': 165.0,
+                                                        'amount': 0.2340606,
+                                                        'fee': {'cost': 0.06179, 'currency': 'BTC'}
+                                                        }])
+    exchange = get_patched_exchange(mocker, default_conf, api_mock)
+
+    orders = exchange.get_trades_for_order(order_id, 'LTC/BTC', since)
+    assert len(orders) == 1
+    assert orders[0]['price'] == 165
+
+    # test Exceptions
+    api_mock = MagicMock()
+    api_mock.fetch_my_trades = MagicMock(side_effect=ccxt.BaseError)
+    exchange = get_patched_exchange(mocker, default_conf, api_mock)
+    with pytest.raises(OperationalException):
+        exchange.get_trades_for_order(order_id, 'LTC/BTC', since)
+
+    api_mock = MagicMock()
+    api_mock.fetch_my_trades = MagicMock(side_effect=ccxt.NetworkError)
+    exchange = get_patched_exchange(mocker, default_conf, api_mock)
+    with pytest.raises(TemporaryError):
+        exchange.get_trades_for_order(order_id, 'LTC/BTC', since)
+    assert api_mock.fetch_my_trades.call_count == API_RETRY_COUNT + 1
 
 
 def test_get_fee(default_conf, mocker):
