6e16c1d80d5173bc855a20f0e142805c9df004fa
Matthias <xmatthias@outlook.com>
2018-07-04 21:42:17 +0200

    add webhook test file

---
+++ freqtrade/tests/rpc/test_rpc_webhook.py
@@ -0,0 +1,16 @@
+
+
+def test__init__():
+    # TODO: Implement me
+    pass
+
+
+def test_send_msg():
+    # TODO: Implement me
+    pass
+
+
+def test__send_msg():
+    """Test internal method - calling the actual api"""
+    # TODO: Implement me
+    pass
