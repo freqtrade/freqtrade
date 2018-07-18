2f905cb6968423ffb927492304714dace60a4ef6
Matthias <xmatthias@outlook.com>
2018-07-18 21:34:04 +0200

    Update test-strategy with new methods

--- user_data/strategies/test_strategy.py
+++ user_data/strategies/test_strategy.py
@@ -44,7 +44,7 @@ class TestStrategy(IStrategy):
     # Optimal ticker interval for the strategy
     ticker_interval = '5m'
 
-    def populate_indicators(self, dataframe: DataFrame) -> DataFrame:
+    def advise_indicators(self, dataframe: DataFrame, pair: str) -> DataFrame:
         """
         Adds several different TA indicators to the given DataFrame
 
@@ -211,7 +211,7 @@ class TestStrategy(IStrategy):
 
         return dataframe
 
-    def populate_buy_trend(self, dataframe: DataFrame) -> DataFrame:
+    def advise_buy(self, dataframe: DataFrame, pair: str) -> DataFrame:
         """
         Based on TA indicators, populates the buy signal for the given dataframe
         :param dataframe: DataFrame
@@ -227,7 +227,7 @@ class TestStrategy(IStrategy):
 
         return dataframe
 
-    def populate_sell_trend(self, dataframe: DataFrame) -> DataFrame:
+    def advise_sell(self, dataframe: DataFrame, pair: str) -> DataFrame:
         """
         Based on TA indicators, populates the sell signal for the given dataframe
         :param dataframe: DataFrame
