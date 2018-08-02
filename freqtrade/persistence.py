85c73ea8507d012353bb9744b4371d978bf07af6
Gert <berlinguyinca@gmail.com>
2018-08-02 16:39:13 -0700

    added index

--- freqtrade/persistence.py
+++ freqtrade/persistence.py
@@ -157,8 +157,8 @@ class Trade(_DECL_BASE):
 
     id = Column(Integer, primary_key=True)
     exchange = Column(String, nullable=False)
-    pair = Column(String, nullable=False)
-    is_open = Column(Boolean, nullable=False, default=True)
+    pair = Column(String, nullable=False,index=True)
+    is_open = Column(Boolean, nullable=False, default=True, index=True)
     fee_open = Column(Float, nullable=False, default=0.0)
     fee_close = Column(Float, nullable=False, default=0.0)
     open_rate = Column(Float)
