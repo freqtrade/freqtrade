a5554604e0e3a9a01582d5221f13e754766d7e87
Axel Cherubin <axel.cherubin@gmail.com>
2018-08-05 16:59:18 -0400

    add sed command in doc, fix travis error

--- install_ta-lib.sh
+++ install_ta-lib.sh
@@ -1,11 +1,7 @@
 if [ ! -f "ta-lib/CHANGELOG.TXT" ]; then
   tar zxvf ta-lib-0.4.0-src.tar.gz
-  cd ta-lib && \
-  sed -i "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h && \
-  ./configure && make && sudo make install && cd ..
+  cd ta-lib && sed -i "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h && ./configure && make && sudo make install && cd ..
 else
   echo "TA-lib already installed, skipping download and build."
-  cd ta-lib && \
-  sed -i "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h && \
-  sudo make install && cd ..
+  cd ta-lib && sed -i "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h && sudo make install && cd ..
 fi
