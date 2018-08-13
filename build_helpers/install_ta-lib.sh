98738c482a7e52ecc9d7d55929e21b54ff2558c9
Matthias <xmatthias@outlook.com>
2018-08-13 20:01:57 +0200

    modify install-ta-lib script to support running in docker

--- build_helpers/install_ta-lib.sh
+++ build_helpers/install_ta-lib.sh
@@ -1,7 +1,13 @@
 if [ ! -f "ta-lib/CHANGELOG.TXT" ]; then
   tar zxvf ta-lib-0.4.0-src.tar.gz
-  cd ta-lib && sed -i.bak "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h && ./configure && make && sudo make install && cd ..
+  cd ta-lib \
+  && sed -i.bak "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h \
+  && ./configure \
+  && make \
+  && which sudo && sudo make install || make install \
+  && cd ..
 else
   echo "TA-lib already installed, skipping download and build."
   cd ta-lib && sudo make install && cd ..
+
 fi
