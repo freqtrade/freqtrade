4bd956d5b1da5f4ab7ed1d3a3e5051c10875a073
misagh <misagh.shakeri@gmail.com>
2018-09-21 17:58:20 +0200

    test file removed

--- freqtrade/edge.py
+++
@@ -1,6 +0,0 @@
-# EDGE 
-# WinRate and Expected Risk Reward should be calculated for all whitelisted pairs
-# ASYNC: For each pair call backslap 
-# Save WR and ERR in Edge Dict
-# Save last time updated for each pair in edge_last_update_time
-# Calulate expectancy and position size and stop loss
