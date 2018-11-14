0767718a179aac64f2ef059cc96f62f8974d4b2d
misagh <misagh.shakeri@gmail.com>
2018-11-14 16:38:55 +0100

    clear help added to stop losses arg

--- freqtrade/arguments.py
+++ freqtrade/arguments.py
@@ -202,7 +202,7 @@ class Arguments(object):
         parser.add_argument(
             '--stoplosses',
             help='defines a range of stoploss against which edge will assess the strategy'
-                 'the format is "min, max, step". example: -0.01, -0.1, -0.001',
+                 'the format is "min,max,step" (without any space). example: --stoplosses=-0.01,-0.1,-0.001',
             type=str,
             dest='stoploss_range',
         )
