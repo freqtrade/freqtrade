36030176bb21ab247e8e27bd1a62a79ce48940ba
misagh <misagh.shakeri@gmail.com>
2018-11-14 13:38:23 +0100

    nb_trades and avg_trade_duration added to cli

--- freqtrade/optimize/edge.py
+++ freqtrade/optimize/edge.py
@@ -53,20 +53,23 @@ class EdgeCli(object):
 
     def _generate_edge_table(self, results: dict) -> str:
 
-        floatfmt = ('s', '.2f', '.2f', '.2f', '.2f', '.2f')
+        floatfmt = ('s', '.2f', '.2f', '.2f', '.2f', '.2f', 'd', '.d')
         tabular_data = []
         headers = ['pair', 'stoploss', 'win rate', 'risk reward ratio',
-                   'required risk reward', 'expectancy']
+                   'required risk reward', 'expectancy', 'total number of trades', 'average duration (min)']
 
         for result in results.items():
-            tabular_data.append([
-                result[0],
-                result[1].stoploss,
-                result[1].winrate,
-                result[1].risk_reward_ratio,
-                result[1].required_risk_reward,
-                result[1].expectancy
-            ])
+            if result[1].nb_trades > 0:
+                tabular_data.append([
+                    result[0],
+                    result[1].stoploss,
+                    result[1].winrate,
+                    result[1].risk_reward_ratio,
+                    result[1].required_risk_reward,
+                    result[1].expectancy,
+                    result[1].nb_trades,
+                    round(result[1].avg_trade_duration)
+                ])
 
         return tabulate(tabular_data, headers=headers, floatfmt=floatfmt, tablefmt="pipe")
 
