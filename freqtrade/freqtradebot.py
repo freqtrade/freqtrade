64129897f9fe696e991af1456f75ddf035f3b799
misagh <misagh.shakeri@gmail.com>
2018-11-21 14:00:15 +0100

    refresh_ticker should be called just once per iteration.

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -194,9 +194,6 @@ class FreqtradeBot(object):
                 self.edge.calculate()
                 self.active_pair_whitelist = self.edge.adjust(self.active_pair_whitelist)
 
-            # Refreshing candles
-            self.exchange.refresh_tickers(self.active_pair_whitelist, self.strategy.ticker_interval)
-
             # Query trades from persistence layer
             trades = Trade.query.filter(Trade.is_open.is_(True)).all()
 
