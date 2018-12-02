ec55f1e45ece6e75fa93fe9117790e3054bcc574
misagh <misagh.shakeri@gmail.com>
2018-12-02 15:38:42 +0100

    slippage to percentage

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -443,9 +443,11 @@ class FreqtradeBot(object):
 
         amount = stake_amount / buy_limit
 
+        last_candle_close = self.exchange.klines[pair][-1][4]
         logger.info('passing order... pair: %s, stake_amount: %s, '
-                    'buy limit: %s, last candle close: %s',
-                    pair_s, stake_amount, buy_limit, self.exchange.klines[pair][-1][4]
+                    'buy limit: %s, last candle close: %s, slippage: %s percent.',
+                    pair_s, stake_amount, buy_limit, last_candle_close,
+                    ((buy_limit / last_candle_close) / last_candle_close) * 100
                     )
 
         order_id = self.exchange.buy(pair=pair, ordertype=self.strategy.order_types['buy'],
