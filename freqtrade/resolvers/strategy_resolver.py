cacb9ef3add0bec62dfcf0ce6ceb9cb9a803f2a0
Matthias <xmatthias@outlook.com>
2019-01-05 07:25:35 +0100

    Loop twice

--- freqtrade/resolvers/strategy_resolver.py
+++ freqtrade/resolvers/strategy_resolver.py
@@ -52,6 +52,9 @@ class StrategyResolver(IResolver):
                       ]
         for attribute in attributes:
             self._override_attribute_helper(config, attribute)
+
+        # Loop this list again to have output combined
+        for attribute in attributes:
             if attribute in config:
                 logger.info("Strategy using %s: %s", attribute, config[attribute])
 
