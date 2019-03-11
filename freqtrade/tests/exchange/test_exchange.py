4705b7da0edb6ee405aba89c59dd42a22b6824cb
Matthias <xmatthias@outlook.com>
2019-03-11 20:30:16 +0100

    Add time_in_force test for sell

--- freqtrade/tests/exchange/test_exchange.py
+++ freqtrade/tests/exchange/test_exchange.py
@@ -590,7 +590,8 @@ def test_buy_prod(default_conf, mocker, exchange_name):
                      amount=1, rate=200, time_in_force=time_in_force)
 
 
-def test_buy_considers_time_in_force(default_conf, mocker):
+@pytest.mark.parametrize("exchange_name", EXCHANGES)
+def test_buy_considers_time_in_force(default_conf, mocker, exchange_name):
     api_mock = MagicMock()
     order_id = 'test_prod_buy_{}'.format(randint(0, 10 ** 6))
     api_mock.create_order = MagicMock(return_value={
@@ -602,7 +603,7 @@ def test_buy_considers_time_in_force(default_conf, mocker):
     default_conf['dry_run'] = False
     mocker.patch('freqtrade.exchange.Exchange.symbol_amount_prec', lambda s, x, y: y)
     mocker.patch('freqtrade.exchange.Exchange.symbol_price_prec', lambda s, x, y: y)
-    exchange = get_patched_exchange(mocker, default_conf, api_mock)
+    exchange = get_patched_exchange(mocker, default_conf, api_mock, id=exchange_name)
 
     order_type = 'limit'
     time_in_force = 'ioc'
@@ -618,7 +619,8 @@ def test_buy_considers_time_in_force(default_conf, mocker):
     assert api_mock.create_order.call_args[0][2] == 'buy'
     assert api_mock.create_order.call_args[0][3] == 1
     assert api_mock.create_order.call_args[0][4] == 200
-    assert api_mock.create_order.call_args[0][5] == {'timeInForce': 'ioc'}
+    assert "timeInForce" in api_mock.create_order.call_args[0][5]
+    assert api_mock.create_order.call_args[0][5]["timeInForce"] == time_in_force
 
     order_type = 'market'
     time_in_force = 'ioc'
@@ -634,7 +636,8 @@ def test_buy_considers_time_in_force(default_conf, mocker):
     assert api_mock.create_order.call_args[0][2] == 'buy'
     assert api_mock.create_order.call_args[0][3] == 1
     assert api_mock.create_order.call_args[0][4] is None
-    assert api_mock.create_order.call_args[0][5] == {}
+    # Market orders should not send timeInForce!!
+    assert "timeInForce" not in api_mock.create_order.call_args[0][5]
 
 
 def test_sell_dry_run(default_conf, mocker):
@@ -703,6 +706,55 @@ def test_sell_prod(default_conf, mocker, exchange_name):
         api_mock.create_order = MagicMock(side_effect=ccxt.BaseError)
         exchange = get_patched_exchange(mocker, default_conf, api_mock, id=exchange_name)
         exchange.sell(pair='ETH/BTC', ordertype=order_type, amount=1, rate=200)
+
+
+@pytest.mark.parametrize("exchange_name", EXCHANGES)
+def test_sell_considers_time_in_force(default_conf, mocker, exchange_name):
+    api_mock = MagicMock()
+    order_id = 'test_prod_sell_{}'.format(randint(0, 10 ** 6))
+    api_mock.create_order = MagicMock(return_value={
+        'id': order_id,
+        'info': {
+            'foo': 'bar'
+        }
+    })
+    default_conf['dry_run'] = False
+    mocker.patch('freqtrade.exchange.Exchange.symbol_amount_prec', lambda s, x, y: y)
+    mocker.patch('freqtrade.exchange.Exchange.symbol_price_prec', lambda s, x, y: y)
+    exchange = get_patched_exchange(mocker, default_conf, api_mock, id=exchange_name)
+
+    order_type = 'limit'
+    time_in_force = 'ioc'
+
+    order = exchange.sell(pair='ETH/BTC', ordertype=order_type,
+                          amount=1, rate=200, time_in_force=time_in_force)
+
+    assert 'id' in order
+    assert 'info' in order
+    assert order['id'] == order_id
+    assert api_mock.create_order.call_args[0][0] == 'ETH/BTC'
+    assert api_mock.create_order.call_args[0][1] == order_type
+    assert api_mock.create_order.call_args[0][2] == 'sell'
+    assert api_mock.create_order.call_args[0][3] == 1
+    assert api_mock.create_order.call_args[0][4] == 200
+    assert "timeInForce" in api_mock.create_order.call_args[0][5]
+    assert api_mock.create_order.call_args[0][5]["timeInForce"] == time_in_force
+
+    order_type = 'market'
+    time_in_force = 'ioc'
+    order = exchange.sell(pair='ETH/BTC', ordertype=order_type,
+                          amount=1, rate=200, time_in_force=time_in_force)
+
+    assert 'id' in order
+    assert 'info' in order
+    assert order['id'] == order_id
+    assert api_mock.create_order.call_args[0][0] == 'ETH/BTC'
+    assert api_mock.create_order.call_args[0][1] == order_type
+    assert api_mock.create_order.call_args[0][2] == 'sell'
+    assert api_mock.create_order.call_args[0][3] == 1
+    assert api_mock.create_order.call_args[0][4] is None
+    # Market orders should not send timeInForce!!
+    assert "timeInForce" not in api_mock.create_order.call_args[0][5]
 
 
 def test_get_balance_dry_run(default_conf, mocker):
