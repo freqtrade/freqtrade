5488c66f536672d22d529450905767c637f5cc5a
Misagh <mishaker.code@gmail.com>
2019-04-03 20:35:37 +0200

    flake8

--- freqtrade/rpc/rpc.py
+++ freqtrade/rpc/rpc.py
@@ -111,11 +111,11 @@ class RPC(object):
                     close_profit=fmt_close_profit,
                     current_profit=round(current_profit * 100, 2),
                     stop_loss=trade.stop_loss,
-                    stop_loss_pct=(trade.stop_loss_pct * 100) \
-                        if trade.stop_loss_pct else None,
+                    stop_loss_pct=(trade.stop_loss_pct * 100)
+                    if trade.stop_loss_pct else None,
                     initial_stop_loss=trade.initial_stop_loss,
-                    initial_stop_loss_pct=(trade.initial_stop_loss_pct * 100) \
-                        if trade.initial_stop_loss_pct else None,
+                    initial_stop_loss_pct=(trade.initial_stop_loss_pct * 100)
+                    if trade.initial_stop_loss_pct else None,
                     open_order='({} {} rem={:.8f})'.format(
                         order['type'], order['side'], order['remaining']
                     ) if order else None,
