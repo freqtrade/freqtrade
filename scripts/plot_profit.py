6913bce6a1e84968bc4added173e82011dc75721
hroff-1902 <hroff_raeyd9tzpttf@mail.ru>
2019-04-04 21:30:08 +0300

    flake8, import in script/plot_profit.py

--- scripts/plot_profit.py
+++ scripts/plot_profit.py
@@ -24,11 +24,10 @@ import plotly.graph_objs as go
 from plotly import tools
 from plotly.offline import plot
 
-from freqtrade import constants, misc
 from freqtrade.arguments import Arguments
 from freqtrade.configuration import Configuration
 from freqtrade.data import history
-from freqtrade.misc import timeframe_to_seconds
+from freqtrade.misc import common_datearray, timeframe_to_seconds
 from freqtrade.resolvers import StrategyResolver
 from freqtrade.state import RunMode
 
@@ -132,7 +131,7 @@ def plot_profit(args: Namespace) -> None:
     # NOTE: the dataframes are of unequal length,
     # 'dates' is an merged date array of them all.
 
-    dates = misc.common_datearray(dataframes)
+    dates = common_datearray(dataframes)
     min_date = int(min(dates).timestamp())
     max_date = int(max(dates).timestamp())
     num_iterations = define_index(min_date, max_date, tick_interval) + 1
