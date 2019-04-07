ebf11263516258f853573468e59c1c9bf0941f34
hroff-1902 <hroff_raeyd9tzpttf@mail.ru>
2019-04-07 16:14:40 +0300

    cosmetic: rename interval, tick_interval, etc --> ticker_interval

--- scripts/plot_profit.py
+++ scripts/plot_profit.py
@@ -76,7 +76,7 @@ def plot_profit(args: Namespace) -> None:
     in helping out to find a good algorithm.
     """
 
-    # We need to use the same pairs, same tick_interval
+    # We need to use the same pairs, same ticker_interval
     # and same timeperiod as used in backtesting
     # to match the tickerdata against the profits-results
     timerange = Arguments.parse_timerange(args.timerange)
@@ -112,7 +112,7 @@ def plot_profit(args: Namespace) -> None:
     else:
         filter_pairs = config['exchange']['pair_whitelist']
 
-    tick_interval = strategy.ticker_interval
+    ticker_interval = strategy.ticker_interval
     pairs = config['exchange']['pair_whitelist']
 
     if filter_pairs:
@@ -122,7 +122,7 @@ def plot_profit(args: Namespace) -> None:
     tickers = history.load_data(
         datadir=Path(str(config.get('datadir'))),
         pairs=pairs,
-        ticker_interval=tick_interval,
+        ticker_interval=ticker_interval,
         refresh_pairs=False,
         timerange=timerange
     )
@@ -134,7 +134,7 @@ def plot_profit(args: Namespace) -> None:
     dates = common_datearray(dataframes)
     min_date = int(min(dates).timestamp())
     max_date = int(max(dates).timestamp())
-    num_iterations = define_index(min_date, max_date, tick_interval) + 1
+    num_iterations = define_index(min_date, max_date, ticker_interval) + 1
 
     # Make an average close price of all the pairs that was involved.
     # this could be useful to gauge the overall market trend
@@ -154,7 +154,7 @@ def plot_profit(args: Namespace) -> None:
     avgclose /= num
 
     # make an profits-growth array
-    pg = make_profit_array(data, num_iterations, min_date, tick_interval, filter_pairs)
+    pg = make_profit_array(data, num_iterations, min_date, ticker_interval, filter_pairs)
 
     #
     # Plot the pairs average close prices, and total profit growth
@@ -178,7 +178,7 @@ def plot_profit(args: Namespace) -> None:
     fig.append_trace(profit, 2, 1)
 
     for pair in pairs:
-        pg = make_profit_array(data, num_iterations, min_date, tick_interval, [pair])
+        pg = make_profit_array(data, num_iterations, min_date, ticker_interval, [pair])
         pair_profit = go.Scattergl(
             x=dates,
             y=pg,
@@ -189,11 +189,11 @@ def plot_profit(args: Namespace) -> None:
     plot(fig, filename=str(Path('user_data').joinpath('freqtrade-profit-plot.html')))
 
 
-def define_index(min_date: int, max_date: int, interval: str) -> int:
+def define_index(min_date: int, max_date: int, ticker_interval: str) -> int:
     """
     Return the index of a specific date
     """
-    interval_seconds = timeframe_to_seconds(interval)
+    interval_seconds = timeframe_to_seconds(ticker_interval)
     return int((max_date - min_date) / interval_seconds)
 
 
