2554ebf2739a7d400c2b766469a92cc6d4d2acd4
hroff-1902 <hroff_raeyd9tzpttf@mail.ru>
2019-05-08 00:00:44 +0300

    fixed: heikinashi worked in backtesting, but failed in tests with testing arrays

--- freqtrade/vendor/qtpylib/indicators.py
+++ freqtrade/vendor/qtpylib/indicators.py
@@ -113,11 +113,15 @@ def heikinashi(bars):
                         bars['low'] + bars['close']) / 4
 
     # ha open
+    idx = bars.index.name
+    bars.reset_index(inplace=True)
     for i in range(0, len(bars)):
         bars.at[i, 'ha_open'] = (
                 (bars.at[0, 'open'] if i == 0 else bars.at[i - 1, 'ha_open']) +
                 (bars.at[0, 'close'] if i == 0 else bars.at[i - 1, 'ha_close'])
             ) / 2
+    if idx:
+        bars.set_index(idx, inplace=True)
 
     bars['ha_high'] = bars.loc[:, ['high', 'ha_open', 'ha_close']].max(axis=1)
     bars['ha_low'] = bars.loc[:, ['low', 'ha_open', 'ha_close']].min(axis=1)
