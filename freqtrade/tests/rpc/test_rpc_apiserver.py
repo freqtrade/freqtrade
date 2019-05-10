b1a14401c2ee83cd3f61ecc6c3a99dac6cd5e9ef
Matthias <xmatthias@outlook.com>
2019-05-10 07:07:38 +0200

    Add some initial tests for apiserver

--- freqtrade/tests/rpc/test_rpc_apiserver.py
+++ freqtrade/tests/rpc/test_rpc_apiserver.py
@@ -4,9 +4,35 @@ Unit test file for rpc/api_server.py
 
 from unittest.mock import MagicMock
 
+import pytest
+
 from freqtrade.rpc.api_server import ApiServer
 from freqtrade.state import State
 from freqtrade.tests.conftest import get_patched_freqtradebot, patch_apiserver
+
+
+@pytest.fixture
+def client(default_conf, mocker):
+    apiserver = ApiServer(get_patched_freqtradebot(mocker, default_conf))
+    yield apiserver.app.test_client()
+    # Cleanup ... ?
+
+
+def response_success_assert(response):
+    assert response.status_code == 200
+    assert response.content_type == "application/json"
+
+
+def test_start(client):
+    rc = client.post("/start")
+    response_success_assert(rc)
+    assert rc.json == {'status': 'already running'}
+
+
+def test_stop(client):
+    rc = client.post("/stop")
+    response_success_assert(rc)
+    assert rc.json == {'status': 'stopping trader ...'}
 
 
 def test__init__(default_conf, mocker):
