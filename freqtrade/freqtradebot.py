349c0619aa30dba6fe38d70575cea7b710b23e27
Matthias <xmatthias@outlook.com>
2019-05-19 20:06:26 +0200

    Move startup to freqtradebot

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -89,6 +89,14 @@ class FreqtradeBot(object):
         self.rpc.cleanup()
         persistence.cleanup()
 
+    def startup(self) -> None:
+        """
+        Called on startup and after reloading the bot - triggers notifications and
+        performs startup tasks
+        : return: None
+        """
+        self.rpc.startup_messages(self.config, self.pairlists)
+
     def process(self) -> bool:
         """
         Queries the persistence layer for open trades and handles them,
