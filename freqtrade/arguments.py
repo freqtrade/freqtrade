b927406c1a51f1224af2609ed651b81ece6c9b0a
hroff-1902 <hroff_raeyd9tzpttf@mail.ru>
2019-07-01 00:58:18 +0300

    remove comment: no longer actual

--- freqtrade/arguments.py
+++ freqtrade/arguments.py
@@ -406,8 +406,6 @@ class Arguments(object):
         """
         parsed_arg = self.parser.parse_args(self.args)
 
-        # Workaround issue in argparse with action='append' and default value
-        # (see https://bugs.python.org/issue16399)
         if not no_default_config and parsed_arg.config is None:
             parsed_arg.config = [constants.DEFAULT_CONFIG]
 
