6c2415d32fba4dfb784f3e0c18652b9eb304fa80
Matthias <xmatthias@outlook.com>
2019-07-07 06:36:35 +0200

    Rename parameters from pair to curr

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -270,14 +270,14 @@ class Exchange(object):
                     f'Pair {pair} is not available on {self.name}. '
                     f'Please remove {pair} from your whitelist.')
 
-    def get_valid_pair_combination(self, paira, pairb) -> str:
+    def get_valid_pair_combination(self, curr_1, curr_2) -> str:
         """
-        Get valid combination of paira and pairb by trying both combinations.
+        Get valid pair combination of curr_1 and curr_2 by trying both combinations.
         """
-        for pair in [f"{paira}/{pairb}", f"{pairb}/{paira}"]:
+        for pair in [f"{curr_1}/{curr_2}", f"{curr_2}/{curr_1}"]:
             if pair in self.markets and self.markets[pair].get('active'):
                 return pair
-        raise DependencyException(f"Could not combine {paira} and {pairb} to get a valid pair.")
+        raise DependencyException(f"Could not combine {curr_1} and {curr_2} to get a valid pair.")
 
     def validate_timeframes(self, timeframe: List[str]) -> None:
         """
