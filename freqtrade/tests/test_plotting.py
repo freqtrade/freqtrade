e4380b533bc4515cd6fc227690d7efb77b945d12
Matthias <xmatthias@outlook.com>
2019-08-04 10:25:46 +0200

    Print plot filename so it can be easily opened

--- freqtrade/tests/test_plotting.py
+++ freqtrade/tests/test_plotting.py
@@ -215,6 +215,8 @@ def test_generate_plot_file(mocker, caplog):
     assert plot_mock.call_args[0][0] == fig
     assert (plot_mock.call_args_list[0][1]['filename']
             == "user_data/plots/freqtrade-plot-UNITTEST_BTC-5m.html")
+    assert log_has("Stored plot as user_data/plots/freqtrade-plot-UNITTEST_BTC-5m.html",
+                   caplog.record_tuples)
 
 
 def test_add_profit():
