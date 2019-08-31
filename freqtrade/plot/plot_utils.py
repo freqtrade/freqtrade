f278fcfc3fb41b2cc9be482824699bc875bd8bfa
Matthias <xmatthias@outlook.com>
2019-08-31 15:14:57 +0200

    Use plot-runmode for plot scripts

--- freqtrade/plot/plot_utils.py
+++ freqtrade/plot/plot_utils.py
@@ -10,7 +10,7 @@ def start_plot_dataframe(args: Namespace) -> None:
     """
     # Import here to avoid errors if plot-dependencies are not installed.
     from freqtrade.plot.plotting import analyse_and_plot_pairs
-    config = setup_utils_configuration(args, RunMode.OTHER)
+    config = setup_utils_configuration(args, RunMode.PLOT)
 
     analyse_and_plot_pairs(config)
 
@@ -21,6 +21,6 @@ def start_plot_profit(args: Namespace) -> None:
     """
     # Import here to avoid errors if plot-dependencies are not installed.
     from freqtrade.plot.plotting import plot_profit
-    config = setup_utils_configuration(args, RunMode.OTHER)
+    config = setup_utils_configuration(args, RunMode.PLOT)
 
     plot_profit(config)
