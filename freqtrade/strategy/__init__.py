13932f55f576b5f31198f290310c3e07357611e5
Matthias <xmatthias@outlook.com>
2019-09-08 14:02:32 +0200

    Fix random test failures

--- freqtrade/strategy/__init__.py
+++ freqtrade/strategy/__init__.py
@@ -1,3 +1,1 @@
 from freqtrade.strategy.interface import IStrategy  # noqa: F401
-# Import Default-Strategy to have hyperopt correctly resolve
-from freqtrade.strategy.default_strategy import DefaultStrategy  # noqa: F401
