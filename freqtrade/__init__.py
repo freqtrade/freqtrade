85f1291597b507a997b62e750c66444afad2ece5
Matthias <xmatthias@outlook.com>
2019-09-09 20:18:47 +0200

    use git log to print version

--- freqtrade/__init__.py
+++ freqtrade/__init__.py
@@ -5,8 +5,9 @@ if __version__ == 'develop':
 
     try:
         import subprocess
-        __version__ = str(subprocess.check_output(
-            ["git", "describe"], stderr=subprocess.DEVNULL).rstrip())
+        __version__ = 'develop-' + subprocess.check_output(
+            ['git', 'log', '--format="%h"', '-n 1'],
+            stderr=subprocess.DEVNULL).decode("utf-8").rstrip().strip('"')
     except Exception:
         # git not available, ignore
         pass
