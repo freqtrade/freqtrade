44289e4c586324e1026ad0de03e1ef3c746128bd
Matthias <xmatthias@outlook.com>
2019-10-30 15:55:35 +0100

    Allow not using files from user_dir

--- freqtrade/resolvers/pairlist_resolver.py
+++ freqtrade/resolvers/pairlist_resolver.py
@@ -40,7 +40,7 @@ class PairListResolver(IResolver):
         current_path = Path(__file__).parent.parent.joinpath('pairlist').resolve()
 
         abs_paths = self.build_search_paths(config, current_path=current_path,
-                                            user_subdir='pairlist', extra_dir=None)
+                                            user_subdir=None, extra_dir=None)
 
         pairlist = self._load_object(paths=abs_paths, object_type=IPairList,
                                      object_name=pairlist_name, kwargs=kwargs)
