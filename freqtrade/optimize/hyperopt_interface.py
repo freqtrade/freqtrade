641e3fdf7a51e846dafd862bc52b071d0000f55c
hroff-1902 <hroff_raeyd9tzpttf@mail.ru>
2019-12-10 03:13:45 +0300

    Fix generation of hyperopt trailing params

--- freqtrade/optimize/hyperopt_interface.py
+++ freqtrade/optimize/hyperopt_interface.py
@@ -175,6 +175,19 @@ class IHyperOpt(ABC):
         ]
 
     @staticmethod
+    def generate_trailing_params(params: Dict) -> Dict:
+        """
+        Create dict with trailing stop parameters.
+        """
+        return {
+            'trailing_stop': params['trailing_stop'],
+            'trailing_stop_positive': params['trailing_stop_positive'],
+            'trailing_stop_positive_offset': (params['trailing_stop_positive'] +
+                                              params['trailing_stop_positive_offset_p1']),
+            'trailing_only_offset_is_reached': params['trailing_only_offset_is_reached'],
+        }
+
+    @staticmethod
     def trailing_space() -> List[Dimension]:
         """
         Create a trailing stoploss space.
@@ -190,8 +203,15 @@ class IHyperOpt(ABC):
             # other 'trailing' hyperspace parameters.
             Categorical([True], name='trailing_stop'),
 
-            Real(0.02, 0.35, name='trailing_stop_positive'),
-            Real(0.01, 0.1, name='trailing_stop_positive_offset'),
+            Real(0.01, 0.35, name='trailing_stop_positive'),
+
+            # 'trailing_stop_positive_offset' should be greater than 'trailing_stop_positive',
+            # so this intermediate parameter is used as the value of the difference between
+            # them. The value of the 'trailing_stop_positive_offset' is constructed in the
+            # generate_trailing_params() method.
+            # # This is similar to the hyperspace dimensions used for constructing the ROI tables.
+            Real(0.001, 0.1, name='trailing_stop_positive_offset_p1'),
+
             Categorical([True, False], name='trailing_only_offset_is_reached'),
         ]
 
