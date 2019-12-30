024aa3ab6b6e5e0ba11bf7cfe0035cb83423bbf3
Matthias <xmatthias@outlook.com>
2019-12-30 14:57:26 +0100

    Move exceptions to seperate module

---
+++ freqtrade/exceptions.py
@@ -0,0 +1,31 @@
+
+
+class DependencyException(Exception):
+    """
+    Indicates that an assumed dependency is not met.
+    This could happen when there is currently not enough money on the account.
+    """
+
+
+class OperationalException(Exception):
+    """
+    Requires manual intervention and will usually stop the bot.
+    This happens when an exchange returns an unexpected error during runtime
+    or given configuration is invalid.
+    """
+
+
+class InvalidOrderException(Exception):
+    """
+    This is returned when the order is not valid. Example:
+    If stoploss on exchange order is hit, then trying to cancel the order
+    should return this exception.
+    """
+
+
+class TemporaryError(Exception):
+    """
+    Temporary network or exchange related error.
+    This could happen when an exchange is congested, unavailable, or the user
+    has networking problems. Usually resolves itself after a time.
+    """
