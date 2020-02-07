aa2cb937b145f7a87b8044fe17e4d945c617ba2c
Yazeed Al Oyoun <yazeed.aloyoun@gmail.com>
2020-02-07 03:54:47 +0100

    flake8 :)

--- tests/optimize/test_optimize_reports.py
+++ tests/optimize/test_optimize_reports.py
@@ -22,10 +22,14 @@ def test_generate_text_table(default_conf, mocker):
     )
 
     result_str = (
-        '| Pair    |   Buys |   Avg Profit % |   Cum Profit % |   Tot Profit BTC |   Tot Profit % | Avg Duration   |   Wins |   Draws |   Losses |\n'
-        '|:--------|-------:|---------------:|---------------:|-----------------:|---------------:|:---------------|-------:|--------:|---------:|\n'
-        '| ETH/BTC |      2 |          15.00 |          30.00 |       0.60000000 |          15.00 | 0:20:00        |      2 |       0 |        0 |\n'
-        '| TOTAL   |      2 |          15.00 |          30.00 |       0.60000000 |          15.00 | 0:20:00        |      2 |       0 |        0 |'
+        '| Pair    |   Buys |   Avg Profit % |   Cum Profit % |   Tot Profit BTC |'
+        '   Tot Profit % | Avg Duration   |   Wins |   Draws |   Losses |\n'
+        '|:--------|-------:|---------------:|---------------:|-----------------:|'
+        '---------------:|:---------------|-------:|--------:|---------:|\n'
+        '| ETH/BTC |      2 |          15.00 |          30.00 |       0.60000000 |'
+        '          15.00 | 0:20:00        |      2 |       0 |        0 |\n'
+        '| TOTAL   |      2 |          15.00 |          30.00 |       0.60000000 |'
+        '          15.00 | 0:20:00        |      2 |       0 |        0 |'
     )
     assert generate_text_table(data={'ETH/BTC': {}},
                                stake_currency='BTC', max_open_trades=2,
@@ -48,10 +52,14 @@ def test_generate_text_table_sell_reason(default_conf, mocker):
     )
 
     result_str = (
-        '| Sell Reason   |   Sells |   Wins |   Draws |   Losses |   Avg Profit % |   Cum Profit % |   Tot Profit BTC |   Tot Profit % |\n'
-        '|:--------------|--------:|-------:|--------:|---------:|---------------:|---------------:|-----------------:|---------------:|\n'
-        '| roi           |       2 |      2 |       0 |        0 |             15 |             30 |              0.6 |             15 |\n'
-        '| stop_loss     |       1 |      0 |       0 |        1 |            -10 |            -10 |             -0.2 |             -5 |'
+        '| Sell Reason   |   Sells |   Wins |   Draws |   Losses |'
+        '   Avg Profit % |   Cum Profit % |   Tot Profit BTC |   Tot Profit % |\n'
+        '|:--------------|--------:|-------:|--------:|---------:|'
+        '---------------:|---------------:|-----------------:|---------------:|\n'
+        '| roi           |       2 |      2 |       0 |        0 |'
+        '             15 |             30 |              0.6 |             15 |\n'
+        '| stop_loss     |       1 |      0 |       0 |        1 |'
+        '            -10 |            -10 |             -0.2 |             -5 |'
     )
     assert generate_text_table_sell_reason(
         data={'ETH/BTC': {}},
@@ -87,10 +95,14 @@ def test_generate_text_table_strategy(default_conf, mocker):
     )
 
     result_str = (
-        '| Strategy      |   Buys |   Avg Profit % |   Cum Profit % |   Tot Profit BTC |   Tot Profit % | Avg Duration   |   Wins |   Draws |   Losses |\n'
-        '|:--------------|-------:|---------------:|---------------:|-----------------:|---------------:|:---------------|-------:|--------:|---------:|\n'
-        '| TestStrategy1 |      3 |          20.00 |          60.00 |       1.10000000 |          30.00 | 0:17:00        |      3 |       0 |        0 |\n'
-        '| TestStrategy2 |      3 |          30.00 |          90.00 |       1.30000000 |          45.00 | 0:20:00        |      3 |       0 |        0 |'
+        '| Strategy      |   Buys |   Avg Profit % |   Cum Profit % |   Tot'
+        ' Profit BTC |   Tot Profit % | Avg Duration   |   Wins |   Draws |   Losses |\n'
+        '|:--------------|-------:|---------------:|---------------:|------'
+        '-----------:|---------------:|:---------------|-------:|--------:|---------:|\n'
+        '| TestStrategy1 |      3 |          20.00 |          60.00 |      '
+        ' 1.10000000 |          30.00 | 0:17:00        |      3 |       0 |        0 |\n'
+        '| TestStrategy2 |      3 |          30.00 |          90.00 |      '
+        ' 1.30000000 |          45.00 | 0:20:00        |      3 |       0 |        0 |'
     )
     assert generate_text_table_strategy('BTC', 2, all_results=results) == result_str
 
