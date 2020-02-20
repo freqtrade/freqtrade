e7b12704dec4c8265e5adf51dfb3d7f36367adae
Fredrik81 <devnull81@gmail.com>
2020-02-20 19:12:55 +0100

    Added test for details

--- tests/commands/test_commands.py
+++ tests/commands/test_commands.py
@@ -780,6 +780,19 @@ def test_hyperopt_list(mocker, capsys, hyperopt_results):
                          " 11/12", " 12/12"])
     args = [
         "hyperopt-list",
+        "--profitable"
+    ]
+    pargs = get_args(args)
+    pargs['config'] = None
+    start_hyperopt_list(pargs)
+    captured = capsys.readouterr()
+    assert all(x in captured.out
+               for x in [" 2/12", " 10/12", "Best result:", "Buy hyperspace params", "Sell hyperspace params", "ROI table", "Stoploss"])
+    assert all(x not in captured.out
+               for x in [" 1/12", " 3/12", " 4/12", " 5/12", " 6/12", " 7/12", " 8/12", " 9/12",
+                         " 11/12", " 12/12"])
+    args = [
+        "hyperopt-list",
         "--no-details",
         "--no-color",
         "--min-trades", "20"
