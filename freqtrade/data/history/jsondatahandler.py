7ecc56fa44d56d3ceaf70cc81b205cbcaff90795
Matthias <xmatthias@outlook.com>
2020-02-22 13:10:41 +0100

    Load ohlcv data as float

--- freqtrade/data/history/jsondatahandler.py
+++ freqtrade/data/history/jsondatahandler.py
@@ -69,7 +69,7 @@ class JsonDataHandler(IDataHandler):
         filename = self._pair_data_filename(self._datadir, pair, timeframe)
         if not filename.exists():
             return DataFrame(columns=self._columns)
-        pairdata = read_json(filename, orient='values')
+        pairdata = read_json(filename, orient='values', dtype='float64')
         pairdata.columns = self._columns
         pairdata['date'] = to_datetime(pairdata['date'],
                                        unit='ms',
