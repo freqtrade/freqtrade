d7373be55344f80883203fa493f766d542911882
Matthias <xmatthias@outlook.com>
2020-02-29 16:58:22 +0100

    Add official support for Kraken

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -1023,7 +1023,7 @@ def is_exchange_known_ccxt(exchange_name: str, ccxt_module: CcxtModuleType = Non
 
 
 def is_exchange_officially_supported(exchange_name: str) -> bool:
-    return exchange_name in ['bittrex', 'binance']
+    return exchange_name in ['bittrex', 'binance', 'kraken']
 
 
 def ccxt_exchanges(ccxt_module: CcxtModuleType = None) -> List[str]:
