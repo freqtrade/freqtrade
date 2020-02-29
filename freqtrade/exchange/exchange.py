60f04cff4de955361b310295b702150e256b8789
Matthias <xmatthias@outlook.com>
2020-02-29 20:41:03 +0100

    Simplify expression

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -333,7 +333,7 @@ class Exchange:
                                f"Please check if you are impacted by this restriction "
                                f"on the exchange and eventually remove {pair} from your whitelist.")
             if (self._config['stake_currency'] and
-                    not self.get_pair_quote_currency(pair) == self._config['stake_currency']):
+                    self.get_pair_quote_currency(pair) != self._config['stake_currency']):
                 invalid_pairs.append(pair)
         if invalid_pairs:
             raise OperationalException(
