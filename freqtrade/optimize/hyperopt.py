92425642da922740e319f9563c93a36873af946f
hroff-1902 <47309513+hroff-1902@users.noreply.github.com>
2020-03-03 01:00:24 +0300

    Fix config_jobs

--- freqtrade/optimize/hyperopt.py
+++ freqtrade/optimize/hyperopt.py
@@ -558,10 +558,6 @@ class Hyperopt:
         cpus = cpu_count()
         logger.info(f"Found {cpus} CPU cores. Let's make them scream!")
         config_jobs = self.config.get('hyperopt_jobs', -1)
-        if (config_jobs < 0 and (cpus + config_jobs + 1) > self.total_epochs) \
-           or (config_jobs > 0 and config_jobs > self.total_epochs):
-            config_jobs = self.total_epochs
-            logger.info("Job count invalid will correct")
         logger.info(f'Number of parallel jobs set as: {config_jobs}')
 
         self.dimensions: List[Dimension] = self.hyperopt_space()
