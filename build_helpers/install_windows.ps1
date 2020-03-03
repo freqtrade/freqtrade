9bebc9ba7699426a86271f711f98e080142531a8
Matthias <xmatthias@outlook.com>
2020-03-03 09:40:41 +0100

    Fix powershell comparison

--- build_helpers/install_windows.ps1
+++ build_helpers/install_windows.ps1
@@ -6,10 +6,10 @@ python -m pip install --upgrade pip
 
 $pyv = python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')"
 
-if ($pyv == '3.7') {
+if ($pyv -eq '3.7') {
     pip install build_helpers\TA_Lib-0.4.17-cp37-cp37m-win_amd64.whl
 }
-if ($pyv == '3.8') {
+if ($pyv -eq '3.8') {
     pip install build_helpers\TA_Lib-0.4.17-cp38-cp38-win_amd64.whl
 }
 
