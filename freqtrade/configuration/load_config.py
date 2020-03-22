45aaa8c09d2a704e49b56ef47b360425f305b7b5
Matthias <xmatthias@outlook.com>
2020-03-22 20:09:01 +0100

    Parse and show relevant configuration section

--- freqtrade/configuration/load_config.py
+++ freqtrade/configuration/load_config.py
@@ -1,18 +1,36 @@
 """
 This module contain functions to load the configuration file
 """
-import rapidjson
 import logging
+import re
 import sys
+from pathlib import Path
 from typing import Any, Dict
 
-from freqtrade.exceptions import OperationalException
+import rapidjson
 
+from freqtrade.exceptions import OperationalException
 
 logger = logging.getLogger(__name__)
 
 
 CONFIG_PARSE_MODE = rapidjson.PM_COMMENTS | rapidjson.PM_TRAILING_COMMAS
+
+
+def log_config_error_range(path: str, errmsg: str) -> str:
+    """
+    Parses configuration file and prints range around error
+    """
+    if path != '-':
+        offsetlist = re.findall(r'(?<=Parse\serror\sat\soffset\s)\d+', errmsg)
+        if offsetlist:
+            offset = int(offsetlist[0])
+            text = Path(path).read_text()
+            # Fetch an offset of 80 characters around the error line
+            subtext = text[offset-min(80, offset):offset+80]
+            segments = subtext.split('\n')
+            # Remove first and last lines, to avoid odd truncations
+            return '\n'.join(segments[1:-1])
 
 
 def load_config_file(path: str) -> Dict[str, Any]:
@@ -29,5 +47,11 @@ def load_config_file(path: str) -> Dict[str, Any]:
         raise OperationalException(
             f'Config file "{path}" not found!'
             ' Please create a config file or check whether it exists.')
+    except rapidjson.JSONDecodeError as e:
+        err_range = log_config_error_range(path, str(e))
+        raise OperationalException(
+            f'{e}\n'
+            f'Please verify the following segment of your configuration:\n{err_range}'
+        )
 
     return config
