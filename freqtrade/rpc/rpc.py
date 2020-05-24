110b5a2521599bcd89ddf86fbf9625ed542fd3b3
Matthias <xmatthias@outlook.com>
2020-05-24 08:46:50 +0200

    Add timestamp to trade output

--- freqtrade/rpc/rpc.py
+++ freqtrade/rpc/rpc.py
@@ -135,9 +135,11 @@ class RPC:
                 trade_dict = trade.to_json()
                 trade_dict.update(dict(
                     base_currency=self._freqtrade.config['stake_currency'],
-                    close_profit=fmt_close_profit,
+                    close_profit=trade.close_profit or None,
+                    close_profit_perc=fmt_close_profit,
                     current_rate=current_rate,
-                    current_profit=round(current_profit * 100, 2),
+                    current_profit_perc=round(current_profit * 100, 2),
+                    current_profit=current_profit,
                     open_order='({} {} rem={:.8f})'.format(
                         order['type'], order['side'], order['remaining']
                     ) if order else None,
--- freqtrade/rpc/telegram.py
+++ freqtrade/rpc/telegram.py
@@ -215,8 +215,8 @@ class Telegram(RPC):
                     "*Open Rate:* `{open_rate:.8f}`",
                     "*Close Rate:* `{close_rate}`" if r['close_rate'] else "",
                     "*Current Rate:* `{current_rate:.8f}`",
-                    "*Close Profit:* `{close_profit}`" if r['close_profit'] else "",
-                    "*Current Profit:* `{current_profit:.2f}%`",
+                    "*Close Profit:* `{close_profit}`" if r['close_profit_perc'] else "",
+                    "*Current Profit:* `{current_profit_perc:.2f}%`",
 
                     # Adding initial stoploss only if it is different from stoploss
                     "*Initial Stoploss:* `{initial_stop_loss:.8f}` " +
