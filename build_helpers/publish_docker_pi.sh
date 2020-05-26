2f596f739d227ec6c7a88a7be3cb4b37e6500964
Matthias <xmatthias@outlook.com>
2020-05-26 17:14:52 +0200

    Use cache correctly

--- build_helpers/publish_docker_pi.sh
+++ build_helpers/publish_docker_pi.sh
@@ -6,18 +6,28 @@
 TAG=$(echo "${BRANCH_NAME}" | sed -e "s/\//_/g")
 PI_PLATFORM="linux/arm/v7"
 echo "Running for ${TAG}"
+CACHE_TAG=${IMAGE_NAME}:${TAG}_cache
 
 # Add commit and commit_message to docker container
 echo "${GITHUB_SHA}" > freqtrade_commit
 
 if [ "${GITHUB_EVENT_NAME}" = "schedule" ]; then
     echo "event ${GITHUB_EVENT_NAME}: full rebuild - skipping cache"
-    docker buildx build -f Dockerfile.armhf --platform ${PI_PLATFORM} -t ${IMAGE_NAME}:${TAG} --push .
+    docker buildx build \
+        --cache-to=type=registry,ref=${CACHE_TAG} \
+        -f Dockerfile.armhf \
+        --platform ${PI_PLATFORM} \
+        -t ${IMAGE_NAME}:${TAG} --push .
 else
     echo "event ${GITHUB_EVENT_NAME}: building with cache"
     # Pull last build to avoid rebuilding the whole image
     # docker pull --platform ${PI_PLATFORM} ${IMAGE_NAME}:${TAG}
-    docker buildx build --cache-from=type=registry,ref=${IMAGE_NAME}:${TAG} -f Dockerfile.armhf --platform ${PI_PLATFORM} -t ${IMAGE_NAME}:${TAG} --push .
+    docker buildx build \
+        --cache-from=type=registry,ref=${CACHE_TAG} \
+        --cache-to=type=registry,ref=${CACHE_TAG} \
+        -f Dockerfile.armhf \
+        --platform ${PI_PLATFORM} \
+        -t ${IMAGE_NAME}:${TAG} --push .
 fi
 
 if [ $? -ne 0 ]; then
