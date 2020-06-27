133947988238905752513992b4ad2caafb5eead3
Matthias <xmatthias@outlook.com>
2020-06-27 07:08:16 +0200

    Have sell_type stringify correctly

--- freqtrade/strategy/interface.py
+++ freqtrade/strategy/interface.py
@@ -45,6 +45,10 @@ class SellType(Enum):
     EMERGENCY_SELL = "emergency_sell"
     NONE = ""
 
+    def __str__(self):
+        # explicitly convert to String to help with exporting data.
+        return self.value
+
 
 class SellCheckTuple(NamedTuple):
     """
