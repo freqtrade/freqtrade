f54fecaebaaa009dd4a5da2c83dcb85516be8a6a
Matthias <xmatthias@outlook.com>
2020-09-02 19:58:26 +0200

    Expose helpermethods thorugh freqtrade.strategy

--- freqtrade/strategy/__init__.py
+++ freqtrade/strategy/__init__.py
@@ -1,1 +1,4 @@
-from freqtrade.strategy.interface import IStrategy  # noqa: F401
+# flake8: noqa: F401
+from freqtrade.exchange import (timeframe_to_minutes, timeframe_to_prev_date,
+                                timeframe_to_seconds, timeframe_to_next_date, timeframe_to_msecs)
+from freqtrade.strategy.interface import IStrategy
