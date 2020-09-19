bfd0e3553accf22775de52e4aec00b038de33116
Matthias <xmatthias@outlook.com>
2020-09-19 08:42:37 +0200

    Don't build this branch anymore in CI

--- freqtrade/exchange/common.py
+++ freqtrade/exchange/common.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 # Maximum default retry count.
 # Functions are always called RETRY_COUNT + 1 times (for the original call)
 API_RETRY_COUNT = 4
-API_FETCH_ORDER_RETRY_COUNT = 3
+API_FETCH_ORDER_RETRY_COUNT = 5
 
 BAD_EXCHANGES = {
     "bitmex": "Various reasons.",
