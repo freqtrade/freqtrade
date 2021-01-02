3dc37dd79d1f8a9af5ffa1a9f4b5b9d46c5093f0
Matthias <xmatthias@outlook.com>
2021-01-02 12:54:40 +0100

    Add types for deps

--- freqtrade/rpc/api_server/deps.py
+++ freqtrade/rpc/api_server/deps.py
@@ -1,13 +1,17 @@
+from typing import Any, Dict
+
+from freqtrade.rpc.rpc import RPC
+
 from .webserver import ApiServer
 
 
-def get_rpc():
+def get_rpc() -> RPC:
     return ApiServer._rpc
 
 
-def get_config():
+def get_config() -> Dict[str, Any]:
     return ApiServer._config
 
 
-def get_api_config():
+def get_api_config() -> Dict[str, Any]:
     return ApiServer._config['api_server']
