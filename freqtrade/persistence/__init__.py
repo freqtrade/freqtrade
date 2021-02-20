87d8b2f93de5931e768e71388cd6c6de898bcb64
Matthias <xmatthias@outlook.com>
2021-02-20 19:29:04 +0100

    2 levels of Trade models, one with and one without sqlalchemy
    Fixes a performance issue when backtesting with sqlalchemy, as that
    uses descriptors for all properties.

--- freqtrade/persistence/__init__.py
+++ freqtrade/persistence/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa: F401
 
-from freqtrade.persistence.models import Order, Trade, clean_dry_run_db, cleanup_db, init_db
+from freqtrade.persistence.models import (LocalTrade, Order, Trade, clean_dry_run_db, cleanup_db,
+                                          init_db)
 from freqtrade.persistence.pairlock_middleware import PairLocks
