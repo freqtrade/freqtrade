5939729e038aa15f3414a27efc1daadde7a10bd1
werkkrew <werkkrew@bytemonkey.net>
2021-03-13 10:45:40 -0500

    added some clarifying comments

--- freqtrade/templates/sample_hyperopt_advanced.py
+++ freqtrade/templates/sample_hyperopt_advanced.py
@@ -274,6 +274,10 @@ class AdvancedSampleHyperOpt(IHyperOpt):
         Create a dynamic ROI space.
 
         You may override it in your custom Hyperopt class.
+
+        If you are reducing the types, you may also remove the parameters that will not be used on the 
+        reduced scope.  For example, if you reduce the types to only 'connect' you do not need to specify
+        the ranges for decay-rate, decay-time, start, or end.
         """        
         return [
             Categorical([True, False], name='enabled'),
