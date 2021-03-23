f51f4b1817efd2451d4877478312f22d9acabefe
rextea <amir.friedmanz@gmail.com>
2021-03-23 10:35:46 +0200

    Add confirm_trade_exit and confirm_trade_entry to backtesting

--- freqtrade/optimize/backtesting.py
+++ freqtrade/optimize/backtesting.py
@@ -255,7 +255,8 @@ class Backtesting:
         time_in_force = self.strategy.order_time_in_force['sell']
         # confirm_trade_exit
         if not strategy_safe_wrapper(self.strategy.confirm_trade_exit, default_retval=True)(
-                pair=trade.pair, trade=trade, order_type='limit', amount=trade.amount, rate=sell_row[LOW_IDX],
+                pair=trade.pair, trade=trade, order_type='limit', amount=trade.amount,
+                rate=sell_row[LOW_IDX],
                 time_in_force=time_in_force,
                 sell_reason=sell.sell_type.value):
             return None
