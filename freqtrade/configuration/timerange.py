7d6c28285931563c12bef8c7edcec2938ebaf980
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2021-03-26 14:27:50 -0700

    Remove "to_datetime" helper method. This can be done explicitly when necessary.

--- freqtrade/configuration/timerange.py
+++ freqtrade/configuration/timerange.py
@@ -34,10 +34,6 @@ class TimeRange:
         return (self.starttype == other.starttype and self.stoptype == other.stoptype
                 and self.startts == other.startts and self.stopts == other.stopts)
 
-    def to_datetime(self) -> (datetime, datetime):
-        return datetime.fromtimestamp(self.startts, timezone.utc), \
-               datetime.fromtimestamp(self.stopts, timezone.utc)
-
     def subtract_start(self, seconds: int) -> None:
         """
         Subtracts <seconds> from startts if startts is set.
