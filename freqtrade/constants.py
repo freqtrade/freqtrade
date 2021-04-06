6f02acdbbd8d1f524e552ae9e24a775fbe7071b1
gbojen <gbojen@kpmg.com>
2021-04-06 10:39:27 +0200

    Revert "resolves freqtrade/freqtrade#4650"
    This reverts commit 142690c93068be58937aaef08dd9004871f237fb.

--- freqtrade/constants.py
+++ freqtrade/constants.py
@@ -26,7 +26,7 @@ HYPEROPT_LOSS_BUILTIN = ['ShortTradeDurHyperOptLoss', 'OnlyProfitHyperOptLoss',
 AVAILABLE_PAIRLISTS = ['StaticPairList', 'VolumePairList',
                        'AgeFilter', 'PerformanceFilter', 'PrecisionFilter',
                        'PriceFilter', 'RangeStabilityFilter', 'ShuffleFilter',
-                       'SpreadFilter', 'VolatilityFilter']
+                       'SpreadFilter']
 AVAILABLE_PROTECTIONS = ['CooldownPeriod', 'LowProfitPairs', 'MaxDrawdown', 'StoplossGuard']
 AVAILABLE_DATAHANDLERS = ['json', 'jsongz', 'hdf5']
 DRY_RUN_WALLET = 1000
