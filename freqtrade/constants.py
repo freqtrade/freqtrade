f37fbbf4e115c3373ad6ef0df660ace3c3c7b83f
klara31 <klara31@europe.com>
2021-04-06 19:47:48 +0200

    Update constants.py

--- freqtrade/constants.py
+++ freqtrade/constants.py
@@ -176,7 +176,7 @@ CONF_SCHEMA = {
                 'order_book_max': {'type': 'integer', 'minimum': 1, 'maximum': 50},
                 'use_sell_signal': {'type': 'boolean'},
                 'sell_profit_only': {'type': 'boolean'},
-                'sell_profit_offset': {'type': 'number', 'minimum': -100},
+                'sell_profit_offset': {'type': 'number'},
                 'ignore_roi_if_buy_signal': {'type': 'boolean'}
             }
         },
