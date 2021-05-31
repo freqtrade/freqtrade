506bdf66cf753a88fef29b5d8156326cda48134d
Jirawat Boonkumnerd <jo06942@gmail.com>
2021-05-31 22:33:20 +0700

    fix: new line for pairlists whitelist telegram cmd

--- freqtrade/rpc/telegram.py
+++ freqtrade/rpc/telegram.py
@@ -854,7 +854,7 @@ class Telegram(RPCHandler):
             message = f"Using whitelist `{whitelist['method']}` with {whitelist['length']} pairs\n"
             message += f"`{', '.join(whitelist['whitelist'])}`"
             if whitelist['pairlistsWhitelist']:
-                message += f"`from current pairlists whitelist {', '.join(whitelist['pairlistsWhitelist'])}`"
+                message += f"`\nfrom current pairlists whitelist {', '.join(whitelist['pairlistsWhitelist'])}`"
 
             logger.debug(message)
             self._send_msg(message)
