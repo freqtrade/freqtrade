55c070f1bb3ed63871e74883c418c88717d1d168
Sam Germain <sam.germain@usask.ca>
2021-08-18 12:43:44 -0600

    Added interface leverage method

--- freqtrade/strategy/interface.py
+++ freqtrade/strategy/interface.py
@@ -816,3 +816,19 @@ class IStrategy(ABC, HyperStrategyMixin):
             return self.populate_sell_trend(dataframe)  # type: ignore
         else:
             return self.populate_sell_trend(dataframe, metadata)
+
+    def leverage(self, pair: str, current_time: datetime, current_rate: float,
+                 proposed_leverage: float, max_leverage: float,
+                 **kwargs) -> float:
+        """
+        Customize leverage for each new trade. This method is not called when edge module is
+        enabled.
+
+        :param pair: Pair that's currently analyzed
+        :param current_time: datetime object, containing the current datetime
+        :param current_rate: Rate, calculated based on pricing settings in ask_strategy.
+        :param proposed_leverage: A leverage proposed by the bot.
+        :param max_leverage: Max leverage allowed on this pair
+        :return: A leverage amount, which is between 1.0 and max_leverage.
+        """
+        return 1.0
