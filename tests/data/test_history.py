127f470bc3928b052fc5ac00d85f4422ae4e5f1f
Masoud Azizi <mablue92@gmail.com>
2021-08-20 20:28:10 +0000

    .* ADDED

--- tests/data/test_history.py
+++ tests/data/test_history.py
@@ -133,8 +133,8 @@ def test_load_data_with_new_pair_1min(ohlcv_history_list, mocker, caplog,
     load_pair_history(datadir=tmpdir1, timeframe='1m', pair='MEME/BTC')
     assert file.is_file()
     assert log_has_re(
-        'Download history data for pair: "MEME/BTC" (0/1), timeframe: 1m '
-        'and store in', caplog
+        r'Download history data for pair: "MEME/BTC" \(0/1\), timeframe: 1m '
+        r'and store in .*', caplog
     )
 
 
