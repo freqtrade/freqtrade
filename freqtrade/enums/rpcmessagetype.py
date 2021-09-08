d811a73ec08a95350dd9d3dde3aa86623707478f
Sam Germain <sam.germain@usask.ca>
2021-09-08 00:20:40 -0600

    new rpc message types

--- freqtrade/enums/rpcmessagetype.py
+++ freqtrade/enums/rpcmessagetype.py
@@ -5,12 +5,22 @@ class RPCMessageType(Enum):
     STATUS = 'status'
     WARNING = 'warning'
     STARTUP = 'startup'
+
     BUY = 'buy'
     BUY_FILL = 'buy_fill'
     BUY_CANCEL = 'buy_cancel'
+
     SELL = 'sell'
     SELL_FILL = 'sell_fill'
     SELL_CANCEL = 'sell_cancel'
+
+    SHORT = 'short'
+    SHORT_FILL = 'short_fill'
+    SHORT_CANCEL = 'short_cancel'
+
+    EXIT_SHORT = 'exit_short'
+    EXIT_SHORT_FILL = 'exit_short_fill'
+    EXIT_SHORT_CANCEL = 'exit_short_cancel'
 
     def __repr__(self):
         return self.value
