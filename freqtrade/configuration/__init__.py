3cdd06f5627b7baee7b5ec3ead09e036080e9b66
Matthias <xmatthias@outlook.com>
2021-09-13 19:32:51 +0200

    Add PeriodicCache

--- freqtrade/configuration/__init__.py
+++ freqtrade/configuration/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa: F401
 
+from freqtrade.configuration.PeriodicCache import PeriodicCache
 from freqtrade.configuration.check_exchange import check_exchange, remove_credentials
 from freqtrade.configuration.config_setup import setup_utils_configuration
 from freqtrade.configuration.config_validation import validate_config_consistency
