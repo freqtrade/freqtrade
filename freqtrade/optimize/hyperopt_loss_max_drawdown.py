46c320513aa0b825b370034feba9d6e9f29af312
sid <siddharthaiist@gmail.com>
2021-10-07 08:07:07 +0530

    use profit_abs

--- freqtrade/optimize/hyperopt_loss_max_drawdown.py
+++ freqtrade/optimize/hyperopt_loss_max_drawdown.py
@@ -32,9 +32,9 @@ class MaxDrawDownHyperOptLoss(IHyperOptLoss):
         Uses profit ratio weighted max_drawdown when drawdown is available.
         Otherwise directly optimizes profit ratio.
         """
-        total_profit = results['profit_ratio'].sum()
+        total_profit = results['profit_abs'].sum()
         try:
-            max_drawdown = calculate_max_drawdown(results)
+            max_drawdown = calculate_max_drawdown(results, value_col='profit_abs')
         except ValueError:
             # No losing trade, therefore no drawdown.
             return -total_profit
