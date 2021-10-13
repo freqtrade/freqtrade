aed919a05f352de482caddc1dc199d0ba2bd8e85
Matthias <xmatthias@outlook.com>
2021-10-13 19:54:35 +0200

    Simplify "no-space-configured" error handling by moving it to hyperopt_auto

--- freqtrade/configuration/configuration.py
+++ freqtrade/configuration/configuration.py
@@ -368,9 +368,9 @@ class Configuration:
 
         self._args_to_config(config, argname='hyperopt_show_no_header',
                              logstring='Parameter --no-header detected: {}')
-        
-        self._args_to_config(config, argname="hyperopt_ignore_unparam_space",
-                             logstring="Paramter --ignore-unparameterized-spaces detected: {}")
+
+        self._args_to_config(config, argname="hyperopt_ignore_missing_space",
+                             logstring="Paramter --ignore-missing-space detected: {}")
 
     def _process_plot_options(self, config: Dict[str, Any]) -> None:
 
