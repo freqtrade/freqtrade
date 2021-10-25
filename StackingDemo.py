9f6e4c6c0e3c6e3fc4d2ad80cc4bfb3385b13152
incrementby1 <ph329709@gmail.com>
2021-10-26 00:31:17 +0200

    uncomment

--- StackingDemo.py
+++ StackingDemo.py
@@ -73,8 +73,8 @@ class StackingDemo(IStrategy):
     # Minimal ROI designed for the strategy.
     # This attribute will be overridden if the config file contains "minimal_roi".
     minimal_roi = {
-#        "60": 0.01,
-#        "30": 0.02,
+        "60": 0.01,
+        "30": 0.02,
         "0": 0.001
     }
 
