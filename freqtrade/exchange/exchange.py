44d9a07acd8637478b359ad6f14fa1b1165f3715
Sam Germain <sam.germain@usask.ca>
2021-10-28 07:20:45 -0600

    Fixed _get_funding_fee_dates method

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -1702,7 +1702,8 @@ class Exchange:
             raise OperationalException(e) from e
 
     def _get_funding_fee_dates(self, d1, d2):
-        d1 = datetime(d1.year, d1.month, d1.day, d1.hour)
+        d1_hours = d1.hour + 1 if d1.minute > 0 or (d1.minute == 0 and d1.second > 15) else d1.hour
+        d1 = datetime(d1.year, d1.month, d1.day, d1_hours)
         d2 = datetime(d2.year, d2.month, d2.day, d2.hour)
 
         results = []
