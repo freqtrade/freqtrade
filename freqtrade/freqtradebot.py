2115a3ed125c2dc1310f8b382b5933cd6e32c0df
Theagainmen <24569139+Theagainmen@users.noreply.github.com>
2021-11-05 18:49:10 +0100

    Update warning message open trades
    This shouldn't confuse user when just reloading their bot.

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -193,21 +193,22 @@ class FreqtradeBot(LoggingMixin):
 
     def check_for_open_trades(self):
         """
-        Notify the user when the bot is stopped
+        Notify the user when the bot is stopped (not reloaded)
         and there are still open trades active.
         """
         open_trades = Trade.get_trades([Trade.is_open.is_(True)]).all()
 
         if len(open_trades) != 0:
-            msg = {
-                'type': RPCMessageType.WARNING,
-                'status': f"{len(open_trades)} open trades active.\n\n"
-                          f"Handle these trades manually on {self.exchange.name}, "
-                          f"or '/start' the bot again and use '/stopbuy' "
-                          f"to handle open trades gracefully. \n"
-                          f"{'Trades are simulated.' if self.config['dry_run'] else ''}",
-            }
-            self.rpc.send_msg(msg)
+            if self.state != State.RELOAD_CONFIG:
+                msg = {
+                    'type': RPCMessageType.WARNING,
+                    'status':  f"{len(open_trades)} open trades active.\n\n"
+                               f"Handle these trades manually on {self.exchange.name}, "
+                               f"or '/start' the bot again and use '/stopbuy' "
+                               f"to handle open trades gracefully. \n"
+                               f"{'Note: Trades are simulated (dry run).' if self.config['dry_run'] else ''}",
+                }
+                self.rpc.send_msg(msg)
 
     def _refresh_active_whitelist(self, trades: List[Trade] = []) -> List[str]:
         """
