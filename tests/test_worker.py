61dbb6206fd615c5e7a76f88cf46953781819dfb
Matthias <xmatthias@outlook.com>
2021-12-23 20:33:13 +0100

    Slightly reduce verbosity when reload_conf is issued
    part of #6095

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -43,7 +43,7 @@ def test_worker_stopped(mocker, default_conf, caplog) -> None:
     worker.freqtrade.state = State.STOPPED
     state = worker._worker(old_state=State.RUNNING)
     assert state is State.STOPPED
-    assert log_has('Changing state to: STOPPED', caplog)
+    assert log_has('Changing state from RUNNING to: STOPPED', caplog)
     assert mock_throttle.call_count == 1
 
 
