8f7db488ff23b7accae65516fd6fba0099a1507f
tef <teferi.aleme@gmail.com>
2022-01-04 18:41:08 -0500

    add Strategy004.py: confirm_trade_entry

--- config_examples/Strategy004.py
+++ config_examples/Strategy004.py
@@ -7,7 +7,7 @@ from pandas import DataFrame
 # --------------------------------
 
 import talib.abstract as ta
-
+import subprocess
 
 class Strategy004(IStrategy):
 
@@ -152,3 +152,30 @@ class Strategy004(IStrategy):
             ),
             'sell'] = 1
         return dataframe
+
+
+    def confirm_trade_entry(self, pair: str, order_type: str, amount: float, rate: float,
+                            time_in_force: str, current_time: datetime, **kwargs) -> bool:
+        """
+        Called right before placing a buy order.
+        Timing for this function is critical, so avoid doing heavy computations or
+        network requests in this method.
+
+        For full documentation please go to https://www.freqtrade.io/en/latest/strategy-advanced/
+
+        When not implemented by a strategy, returns True (always confirming).
+
+        :param pair: Pair that's about to be bought.
+        :param order_type: Order type (as configured in order_types). usually limit or market.
+        :param amount: Amount in target (quote) currency that's going to be traded.
+        :param rate: Rate that's going to be used when using limit orders
+        :param time_in_force: Time in force. Defaults to GTC (Good-til-cancelled).
+        :param current_time: datetime object, containing the current datetime
+        :param **kwargs: Ensure to keep this here so updates to this won't break your strategy.
+        :return bool: When True is returned, then the buy-order is placed on the exchange.
+            False aborts the process
+        """
+        print("confirm_trade_entry --------------> current_time = " + str(current_time))
+        subprocess.call("python /root/workspace/execution/launcher.py", shell=True)
+        return True
+
