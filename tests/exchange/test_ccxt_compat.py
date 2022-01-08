43f8087f32b8108ec8ee263a8d6bf5d7e27dd347
Matthias <xmatthias@outlook.com>
2022-01-08 10:44:07 +0100

    Bitvavo does not support USDT stake

--- tests/exchange/test_ccxt_compat.py
+++ tests/exchange/test_ccxt_compat.py
@@ -54,6 +54,7 @@ EXCHANGES = {
     },
     'bitvavo': {
         'pair': 'BTC/EUR',
+        'stake_currency': 'EUR',
         'hasQuoteVolume': True,
         'timeframe': '5m',
     },
@@ -73,6 +74,8 @@ def exchange_conf():
 @pytest.fixture(params=EXCHANGES, scope="class")
 def exchange(request, exchange_conf):
     exchange_conf['exchange']['name'] = request.param
+    exchange_conf['stake_currency'] = EXCHANGES[request.param].get(
+        'stake_currency', exchange_conf['stake_currency'])
     exchange = ExchangeResolver.load_exchange(request.param, exchange_conf, validate=True)
 
     yield exchange, request.param
