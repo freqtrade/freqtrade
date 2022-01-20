76e2be8579b830f4237c647410068209a186b705
Bemhreth <bemhretgezahegn@gmail.com>
2022-01-20 15:48:41 +0300

    fix

--- user_data/strategies/util.py
+++ user_data/strategies/util.py
@@ -8,4 +8,5 @@ def launcher(mode, coin, brain):
     subprocess.call("python3 "+EXECUTION_PATH+"launcher.py " + mode + " " + coin + " " + brain, shell=True)
 
 def back_tester(date_time, coin, brain):
+    date_time = date_time.replace(" ", "#") 	
     subprocess.call("python3 "+EXECUTION_PATH+"back_tester.py " + date_time + " " + coin + " " + brain + "0.45 3", shell=True)
