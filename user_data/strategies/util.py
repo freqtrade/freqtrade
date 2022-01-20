80750cd8ab2b2111cccd28badeb296c9cef8b8db
Bemhreth <bemhretgezahegn@gmail.com>
2022-01-20 22:50:58 +0300

    fixed passing values issue

--- user_data/strategies/util.py
+++ user_data/strategies/util.py
@@ -2,11 +2,12 @@ import subprocess
 
 IS_BACKTEST = False
 
-EXECUTION_PATH = "/root/workspace/execution/"
+EXECUTION_PATH = "/root/workspace2/execution/"
 
 def launcher(mode, coin, brain):
     subprocess.call("python3 "+EXECUTION_PATH+"launcher.py " + mode + " " + coin + " " + brain, shell=True)
 
 def back_tester(date_time, coin, brain):
-    date_time = date_time.replace(" ", "#") 	
-    subprocess.call("python3 "+ EXECUTION_PATH + "back_tester.py " + date_time + " " + coin + " " + brain + "0.45 3", shell=True)
+    date = str(date_time)
+    date = date.replace(" ", "#")
+    subprocess.call("python3 "+ EXECUTION_PATH + "back_tester.py " + date + " " + coin + " " + brain + " 0.45 3", shell=True)
