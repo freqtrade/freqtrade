d02acb21c2ea6bb53786619193c8da226a0174e6
Matthias <xmatthias@outlook.com>
2022-01-21 07:04:25 +0100

    Add simple test for #buy header

--- freqtrade/rpc/rpc.py
+++ freqtrade/rpc/rpc.py
@@ -242,7 +242,7 @@ class RPC:
                     trade.id,
                     trade.pair + ('*' if (trade.open_order_id is not None
                                           and trade.close_rate_requested is None) else '')
-                               + ('**' if (trade.close_rate_requested is not None) else ''),
+                    + ('**' if (trade.close_rate_requested is not None) else ''),
                     shorten_date(arrow.get(trade.open_date).humanize(only_distance=True)),
                     profit_str
                 ]
