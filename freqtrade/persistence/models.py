51b94889b27e7453b815afb2a988cee1469481af
Reigo Reinmets <reigo@reinmets.ee>
2022-01-23 17:56:41 +0200

    Just in case also check for closed to avoid counting in canceled orders.

--- freqtrade/persistence/models.py
+++ freqtrade/persistence/models.py
@@ -587,7 +587,7 @@ class LocalTrade():
             tmp_price = o.average or o.price or 0.0
             if o.filled is not None:
                 tmp_amount = o.filled
-            if tmp_amount > 0.0 and tmp_price is not None:
+            if tmp_amount > 0.0 and tmp_price is not None and o.status == 'closed':
                 total_amount += tmp_amount
                 total_stake += tmp_price * tmp_amount
 
