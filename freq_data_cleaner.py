12fdc75745de60cbd4b6fd5f013059b7691ce393
Bemhreth <bemhretgezahegn@gmail.com>
2022-01-23 16:59:46 +0300

    restructured

--- freq_data_cleaner.py
+++ freq_data_cleaner.py
@@ -3,11 +3,11 @@ import datetime
 import time
 import os
 import sys
-from util import BACKTEST_JSON_PATH, BACKTEST_YEAR, BACKTEST_MONTH
+from user_data.strategies.util import BACKTEST_DOWNLOADED_JSON_DATA_FILE_PATH, BACKTEST_YEAR, BACKTEST_MONTH
 
 def clean_json():
-    print("clean_json: json_path = " + BACKTEST_JSON_PATH)
-    file = open(BACKTEST_JSON_PATH)
+    print("clean_json: json_path = " + BACKTEST_DOWNLOADED_JSON_DATA_FILE_PATH)
+    file = open(BACKTEST_DOWNLOADED_JSON_DATA_FILE_PATH)
     list = []
     data = json.load(file)
     for datas in data:
@@ -24,17 +24,17 @@ def clean_json():
     write_to_json(json_object)
 
 def write_to_json(json_object):  
-    print("write_to_json: json_path = " + BACKTEST_JSON_PATH)  
+    print("write_to_json: json_path = " + BACKTEST_DOWNLOADED_JSON_DATA_FILE_PATH)  
     with open("temp.json", "w") as outfile:
         outfile.write(json_object)
-    os.rename("temp.json", BACKTEST_JSON_PATH)
+    os.rename("temp.json", BACKTEST_DOWNLOADED_JSON_DATA_FILE_PATH)
 
 if len(sys.argv) < 4:
     exit("""Incorrect number of arguments. 
     python3 freq_data_cleaner.py [json_file] [month] [year]
     """)
 else:
-    BACKTEST_JSON_PATH = sys.argv[1]
+    BACKTEST_DOWNLOADED_JSON_DATA_FILE_PATH = sys.argv[1]
     BACKTEST_MONTH = sys.argv[2]
     BACKTEST_YEAR = sys.argv[3]
     clean_json()
