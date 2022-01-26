8fb82dd215fdf5e4121f5430c9e785b2d5ccb0b0
Bemhreth <bemhretgezahegn@gmail.com>
2022-01-26 16:57:03 +0300

    restructured

--- user_data/strategies/util.py
+++ user_data/strategies/util.py
@@ -6,7 +6,7 @@ def execute(mode, coin, brain):
     if Config.IS_PARRALER_EXECUTION:
         threading.Thread(target=_perform_execute, args=(mode, coin, brain)).start()
     else:
-        subprocess.call("python3 "+Config.EXECUTION_PATH+"executeer.py " + mode + " " + coin + " " + brain, shell=True)
+        _perform_execute(mode, coin, brain)
 
 def _perform_execute(mode, coin, brain):
         subprocess.call("python3 "+Config.EXECUTION_PATH+"executeer.py " + mode + " " + coin + " " + brain, shell=True)
@@ -20,6 +20,4 @@ def back_test(date_time, coin, brain):
     if Config.IS_PARRALER_EXECUTION:
         threading.Thread(target=_perform_back_test, args=(coin, brain, date_time)).start()
     else:
-        date = str(date_time)
-        date = date.replace(" ", "#")
-        subprocess.call("python3 "+ Config.EXECUTION_PATH + "back_tester.py " + date + " " + coin + " " + brain + " 0.45 3", shell=True)     
+        _perform_back_test(date_time, coin, brain)
