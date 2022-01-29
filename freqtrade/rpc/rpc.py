d1d520769ebf60083e4db6bd569acd46a7ac3f3e
Matthias <xmatthias@outlook.com>
2022-01-29 14:25:33 +0100

    Improve status table for position adjust
    don't show "/max" if no maximum is set
    
    closes #6317

--- freqtrade/rpc/rpc.py
+++ freqtrade/rpc/rpc.py
@@ -252,9 +252,11 @@ class RPC:
                     profit_str
                 ]
                 if self._config.get('position_adjustment_enable', False):
-                    max_buy = self._config['max_entry_position_adjustment'] + 1
+                    max_buy_str = ''
+                    if self._config.get('max_entry_position_adjustment', -1) > 0:
+                        max_buy_str = f"/{self._config['max_entry_position_adjustment'] + 1}"
                     filled_buys = trade.nr_of_successful_buys
-                    detail_trade.append(f"{filled_buys}/{max_buy}")
+                    detail_trade.append(f"{filled_buys}{max_buy_str}")
                 trades_list.append(detail_trade)
             profitcol = "Profit"
             if self._fiat_converter:
