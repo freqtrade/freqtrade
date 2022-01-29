d133a7c7892ac73920b58dec43593bad02a5c944
Sam Germain <sam.germain@usask.ca>
2022-01-28 21:57:34 -0600

    added isolated, futures to okex trading_mode_collateral_pairs

--- freqtrade/exchange/okex.py
+++ freqtrade/exchange/okex.py
@@ -25,5 +25,5 @@ class Okex(Exchange):
         # TradingMode.SPOT always supported and not required in this list
         # (TradingMode.MARGIN, Collateral.CROSS),
         # (TradingMode.FUTURES, Collateral.CROSS),
-        # (TradingMode.FUTURES, Collateral.ISOLATED)
+        (TradingMode.FUTURES, Collateral.ISOLATED)
     ]
