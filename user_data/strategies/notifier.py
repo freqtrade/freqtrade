f349a4abc6cab12a968f163b695db42143ff2c77
Bemhreth <bemhretgezahegn@gmail.com>
2022-02-01 22:51:23 +0300

    capiitalization fix

--- user_data/strategies/notifier.py
+++ user_data/strategies/notifier.py
@@ -3,7 +3,7 @@ from user_data.strategies.config import Config
 
 
 def send_backtest_start_deliminator_message(action, coin, month, year):
-    print("Notifier: send_backtest_start_deliminator_message: action= " + str(action))
+    print("notifier: send_backtest_start_deliminator_message: action= " + str(action))
     text = "=========" + str(coin) + " " + str(month) + " " + str(year) + "=========>"
 
     result = requests.post('https://api.telegram.org/bot' + Config.NOTIFIER_TELEGRAM_BOT_API_TOKEN_BACKTEST +
