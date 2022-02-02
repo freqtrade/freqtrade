cdee253fdaf461113c1a4555206688d30c6ad520
Bemhreth <bemhretgezahegn@gmail.com>
2022-02-02 10:54:53 +0300

    fixes implemented

--- user_data/strategies/notifier.py
+++ user_data/strategies/notifier.py
@@ -4,8 +4,8 @@ sys.path.append('/root/workspace2/execution/')
 from config import Config
 
 
-def send_backtest_start_deliminator_message(action, coin, month, year):
-    print("notifier: send_backtest_start_deliminator_message: action= " + str(action))
+def send_start_deliminator_message(action, coin, month, year):
+    print("notifier: send_start_deliminator_message: action= " + str(action))
     text = "=========" + str(coin) + " " + str(month) + " " + str(year) + "=========>"
 
     result = requests.post('https://api.telegram.org/bot' + Config.NOTIFIER_TELEGRAM_BOT_API_TOKEN_BACKTEST +
