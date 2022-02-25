df726a54f89af8f99cb5dd3969ba280187d22d58
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2022-02-25 00:20:53 +0000

    cater for case where sell limit order expired

--- freqtrade/rpc/telegram.py
+++ freqtrade/rpc/telegram.py
@@ -448,7 +448,8 @@ class Telegram(RPCHandler):
                     "*Current Pair:* {pair}",
                     "*Amount:* `{amount} ({stake_amount} {base_currency})`",
                     "*Entry Tag:* `{buy_tag}`" if r['buy_tag'] else "",
-                    "*Exit Reason:* `{sell_reason}`" if r['sell_reason'] else "",
+                    "*Exit Reason:* `{sell_reason}`"
+                    if (r['sell_reason'] and not r['is_open']) else "",
                 ]
 
                 if position_adjust:
