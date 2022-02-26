e25929f50a496e4fe469c5f98598a34a53925b72
Matthias <xmatthias@outlook.com>
2022-02-26 15:53:01 +0100

    Update test to not fail randomly

--- tests/rpc/test_rpc_apiserver.py
+++ tests/rpc/test_rpc_apiserver.py
@@ -610,7 +610,10 @@ def test_api_trade_single(botclient, mocker, fee, ticker, markets, is_short):
     assert rc.json()['detail'] == 'Trade not found.'
 
     create_mock_trades(fee, is_short=is_short)
-    Trade.query.session.flush()
+
+    # The below line avoids random test failures.
+    # It's unclear why.
+    assert len(Trade.get_trades().all()) > 1
 
     rc = client_get(client, f"{BASE_URI}/trade/3")
     assert_response(rc)
