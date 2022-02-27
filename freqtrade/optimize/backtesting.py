6fdcc714bfabb39877e3fdd7bfdda91a55715913
Sam Germain <sam.germain@usask.ca>
2022-02-27 11:59:27 -0600

    backtesting margin_mode key fix

--- freqtrade/optimize/backtesting.py
+++ freqtrade/optimize/backtesting.py
@@ -130,7 +130,7 @@ class Backtesting:
         # TODO-lev: This should come from the configuration setting or better a
         # TODO-lev: combination of config/strategy "use_shorts"(?) and "can_short" from the exchange
         self.trading_mode: TradingMode = config.get('trading_mode', TradingMode.SPOT)
-        self.margin_mode: MarginMode = config.get('trading_mode', MarginMode.NONE)
+        self.margin_mode: MarginMode = config.get('margin_mode', MarginMode.NONE)
         self._can_short = self.trading_mode != TradingMode.SPOT
 
         self.progress = BTProgress()
