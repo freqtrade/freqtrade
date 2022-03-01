b41f425424a8d37b9f92677f284ba3dab0e5ed21
Bemhreth <bemhretgezahegn@gmail.com>
2022-03-02 01:50:37 +0300

    fix

--- user_data/strategies/_429_file_util.py
+++ user_data/strategies/_429_file_util.py
@@ -5,22 +5,22 @@ from user_data.strategies.config import Config
 
 def delete_429_file(text):
     print("delete_429_file:...")
-    # execution_id = text.split('\n')[3].split(':')[1].replace(" ", "").replace("*", "")
-    # action_1 = text.split('\n')[0].split(':')[1].replace(" ", "").replace("*", "")
-    # action_2 = text.split('\n')[0].split(':')[2].replace(" ", "").replace("*", "")
-    # file_name = Config._429_DIRECTORY + execution_id + '_' + action_1 + '_' + action_2
-    # if os.path.isfile(file_name):
-    #     os.remove(file_name)
+    execution_id = text.split('\n')[3].split(':')[1].replace(" ", "").replace("*", "")
+    action_1 = text.split('\n')[0].split(':')[1].replace(" ", "").replace("*", "")
+    action_2 = text.split('\n')[0].split(':')[2].replace(" ", "").replace("*", "")
+    file_name = Config._429_DIRECTORY + execution_id + '_' + action_1 + '_' + action_2
+    if os.path.isfile(file_name):
+        os.remove(file_name)
 
 
 def write_to_429_file(text):
-    # execution_id = text.split('\n')[3].split(':')[1].replace(" ", "").replace("*", "")
-    # action_1 = text.split('\n')[0].split(':')[1].replace(" ", "").replace("*", "")
-    # action_2 = text.split('\n')[0].split(':')[2].replace(" ", "").replace("*", "")
-    # with open(Config._429_DIRECTORY + execution_id + '_' + action_1 + '_' + action_2,
-    #           'w') as file:
-    #     file.write(text)
-    # file.close()
+    execution_id = text.split('\n')[3].split(':')[1].replace(" ", "").replace("*", "")
+    action_1 = text.split('\n')[0].split(':')[1].replace(" ", "").replace("*", "")
+    action_2 = text.split('\n')[0].split(':')[2].replace(" ", "").replace("*", "")
+    with open(Config._429_DIRECTORY + execution_id + '_' + action_1 + '_' + action_2,
+              'w') as file:
+        file.write(text)
+    file.close()
     print(
         "write_to_file: " + Config._429_DIRECTORY + execution_id + '_' + action_1 + '_' + action_2)
 
