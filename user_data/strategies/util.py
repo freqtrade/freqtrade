8f4c134bd882a63a6ec3b1ec426dc6726749ac30
Bemhreth <bemhretgezahegn@gmail.com>
2022-03-02 02:18:04 +0300

    test

--- user_data/strategies/util.py
+++ user_data/strategies/util.py
@@ -1,7 +1,11 @@
+EXECUTION_PATH = '/root/workspace2/execution'
 import subprocess
 import threading
 from user_data.strategies.config import Config
 import time
+import sys
+sys.path.append(EXECUTION_PATH)
+from config import Config as ExecutionConfig
 
 
 def execute(mode, coin, brain):
@@ -23,22 +27,22 @@ def _perform_back_test(date_time, coin, brain):
     #     shell=True)
 
     #todo: uncomment line 26-41, import execution classes, you can import config as ExecutionConfig, Use the variables from line 22 above and test if line 41 works directly
-    # date = sys.argv[1].replace("#", ", ")
-    # Config.COIN = sys.argv[2]
-    # Config.BRAIN = sys.argv[3]
-    # Config.ROMEO_D_UP_PERCENTAGE = float(sys.argv[4])
-    # Config.ROMEO_D_UP_MAX = int(sys.argv[5])
-    # Config.BACKTEST_SIGNAL_TIMESTAMP = get_unix_timestamp(date.split("+", 1)[0])
-    # Config.BACKTEST_MONTH_INDEX = get_month_from_timestamp()
-    # Config.BACKTEST_YEAR = get_year_from_timestamp()
-    # Config.IS_BACKTEST = True
-    # print("back_tester: Config.BACKTEST_SIGNAL_TIMESTAMP = " + str(
-    #     Config.BACKTEST_SIGNAL_TIMESTAMP) + " Config.BACKTEST_MONTH_INDEX = " + str(
-    #     Config.BACKTEST_MONTH_INDEX) + " Config.COIN = " + str(Config.COIN) + " Config.BRAIN = " + str(
-    #     Config.BRAIN) + " Config.ROMEO_D_UP_PERCENTAGE = " + str(
-    #     Config.ROMEO_D_UP_PERCENTAGE) + " Config.ROMEO_D_UP_MAX = " + str(Config.ROMEO_D_UP_MAX))
-    #
-    # Romeo.instance(True)
+    date = sys.argv[1].replace("#", ", ")
+    ExecutionConfig.COIN = sys.argv[2]
+    ExecutionConfig.BRAIN = sys.argv[3]
+    ExecutionConfig.ROMEO_D_UP_PERCENTAGE = float(sys.argv[4])
+    ExecutionConfig.ROMEO_D_UP_MAX = int(sys.argv[5])
+    ExecutionConfig.BACKTEST_SIGNAL_TIMESTAMP = get_unix_timestamp(date.split("+", 1)[0])
+    ExecutionConfig.BACKTEST_MONTH_INDEX = get_month_from_timestamp()
+    ExecutionConfig.BACKTEST_YEAR = get_year_from_timestamp()
+    ExecutionConfig.IS_BACKTEST = True
+    print("back_tester: Config.BACKTEST_SIGNAL_TIMESTAMP = " + str(
+        ExecutionConfig.BACKTEST_SIGNAL_TIMESTAMP) + " Config.BACKTEST_MONTH_INDEX = " + str(
+        ExecutionConfig.BACKTEST_MONTH_INDEX) + " Config.COIN = " + str(Config.COIN) + " Config.BRAIN = " + str(
+        ExecutionConfig.BRAIN) + " Config.ROMEO_D_UP_PERCENTAGE = " + str(
+        ExecutionConfig.ROMEO_D_UP_PERCENTAGE) + " Config.ROMEO_D_UP_MAX = " + str(Config.ROMEO_D_UP_MAX))
+
+    Romeo.instance(True)
 
 
 def back_test(date_time, coin, brain):
