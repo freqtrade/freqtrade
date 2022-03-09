ae4742afcb78d58d30ec88125cdb526204780a62
Sam Germain <sam.germain@usask.ca>
2022-03-09 00:59:28 -0600

    test_fetch_stoploss_order_gateio and test_cancel_stoploss_order_gateio

--- tests/exchange/test_gateio.py
+++ tests/exchange/test_gateio.py
@@ -1,8 +1,11 @@
+from unittest.mock import MagicMock
+
 import pytest
 
 from freqtrade.exceptions import OperationalException
 from freqtrade.exchange import Gateio
 from freqtrade.resolvers.exchange_resolver import ExchangeResolver
+from tests.conftest import get_patched_exchange
 
 
 def test_validate_order_types_gateio(default_conf, mocker):
@@ -26,3 +29,29 @@ def test_validate_order_types_gateio(default_conf, mocker):
     with pytest.raises(OperationalException,
                        match=r'Exchange .* does not support market orders.'):
         ExchangeResolver.load_exchange('gateio', default_conf, True)
+
+
+def test_fetch_stoploss_order_gateio(default_conf, mocker):
+    exchange = get_patched_exchange(mocker, default_conf, id='gateio')
+
+    fetch_order_mock = MagicMock()
+    exchange.fetch_order = fetch_order_mock
+
+    exchange.fetch_stoploss_order('1234', 'ETH/BTC')
+    assert fetch_order_mock.call_count == 1
+    assert fetch_order_mock.call_args_list[0][1]['order_id'] == '1234'
+    assert fetch_order_mock.call_args_list[0][1]['pair'] == 'ETH/BTC'
+    assert fetch_order_mock.call_args_list[0][1]['params'] == {'stop': True}
+
+
+def test_cancel_stoploss_order_gateio(default_conf, mocker):
+    exchange = get_patched_exchange(mocker, default_conf, id='gateio')
+
+    cancel_order_mock = MagicMock()
+    exchange.cancel_order = cancel_order_mock
+
+    exchange.cancel_stoploss_order('1234', 'ETH/BTC')
+    assert cancel_order_mock.call_count == 1
+    assert cancel_order_mock.call_args_list[0][1]['order_id'] == '1234'
+    assert cancel_order_mock.call_args_list[0][1]['pair'] == 'ETH/BTC'
+    assert cancel_order_mock.call_args_list[0][1]['params'] == {'stop': True}
