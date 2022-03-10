6f4d6079023fc75987f7c08087ea4d9e16020387
Sam Germain <sam.germain@usask.ca>
2022-03-09 19:31:51 -0600

    stoploss_adjust fixed breaking tests

--- tests/test_freqtradebot.py
+++ tests/test_freqtradebot.py
@@ -1563,9 +1563,7 @@ def test_tsl_on_exchange_compatible_with_edge(mocker, edge_conf, fee,
         'type': 'stop_loss_limit',
         'price': 3,
         'average': 2,
-        'info': {
-            'stopPrice': '2.178'
-        }
+        'stopPrice': '2.178'
     })
 
     mocker.patch('freqtrade.exchange.Exchange.fetch_stoploss_order', stoploss_order_hanging)
