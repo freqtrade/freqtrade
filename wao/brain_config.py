431d958afb95a27dd9534493c258c205d8bba262
tef-github <teferi.aleme@gmail.com>
2022-03-18 05:22:30 +0100

    remove redundant var

--- wao/brain_config.py
+++ wao/brain_config.py
@@ -9,7 +9,6 @@ class BrainConfig:
     BACKTEST_DATA_CLEANER_MONTH_INDEX = 4
     IS_BACKTEST = False
     WORKSPACE_PATH = "workspace2" if IS_BACKTEST else "workspace"
-    EXECUTION_PATH = "/root/" + WORKSPACE_PATH + "/execution/"
     _429_DIRECTORY = "/root/" + WORKSPACE_PATH + "/freqtrade/_429_directory/"
     IS_PARALLEL_EXECUTION = True
     BACKTEST_THROTTLE_SECOND = 1
