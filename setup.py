84777e255e653a078c307f2e827ccf9c0a582213
Matthias <xmatthias@outlook.com>
2022-03-27 14:58:08 +0200

    Force-bump ccxt version to 1.77.29

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     ],
     install_requires=[
         # from requirements.txt
-        'ccxt>=1.76.5',
+        'ccxt>=1.77.29',
         'SQLAlchemy',
         'python-telegram-bot>=13.4',
         'arrow>=0.17.0',
