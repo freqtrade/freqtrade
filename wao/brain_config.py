6bed267a241e523a4741af3b24a75f84380d0913
teferi <teferi.aleme@gmail.com>
2022-03-29 13:37:15 -0700

    enable parallel execution

--- wao/brain_config.py
+++ wao/brain_config.py
@@ -10,6 +10,6 @@ class BrainConfig:
     IS_BACKTEST = False
     WORKSPACE_PATH = "workspace2" if IS_BACKTEST else "workspace"
     _429_DIRECTORY = "/root/" + WORKSPACE_PATH + "/freqtrade/_429_directory/"
-    IS_PARALLEL_EXECUTION = False
+    IS_PARALLEL_EXECUTION = True
     BACKTEST_THROTTLE_SECOND = 1
     IS_429_FIX_ENABLED = False
