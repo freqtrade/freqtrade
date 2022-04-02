b52f884e3a5b81308628a1d4fc0a6883d5991281
Bemhreth <bemhretgezahegn@gmail.com>
2022-04-02 17:00:26 +0300

    added strategies config file

--- wao/notifier.py
+++ wao/notifier.py
@@ -19,16 +19,16 @@ def send_start_deliminator_message(brain, coin, month, year, dup, max_counter_du
     post_request(text)
 
 
-def post_request(text, is_from_romeo=False):
+def post_request(text, is_from_429_watcher=False):
     print("post_request: " + text)
-    telegram_bot_api_token = Config.NOTIFIER_TELEGRAM_BOT_API_TOKEN_429 if is_from_romeo else Config.NOTIFIER_TELEGRAM_BOT_API_TOKEN_BACKTEST
+    telegram_bot_api_token = Config.NOTIFIER_TELEGRAM_BOT_API_TOKEN_429 if is_from_429_watcher else Config.NOTIFIER_TELEGRAM_BOT_API_TOKEN_BACKTEST
     result = requests.post('https://api.telegram.org/bot' + telegram_bot_api_token +
                            '/sendMessage?chat_id=' + Config.NOTIFIER_TELEGRAM_CHANNEL_ID_BACKTEST +
                            '&text=' + text.replace("_", "-") + '&parse_mode=Markdown')
 
     print(str(result))
 
-    if is_from_romeo:
+    if is_from_429_watcher:
         if str(result) == TELEGRAM_RESPONSE_429:
             delete_429_file(text)
             write_to_429_file(text)
