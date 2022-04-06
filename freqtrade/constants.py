146d6e7c6c87084f6ddb77a1e6f2f80e338972ee
Matthias <xmatthias@outlook.com>
2022-04-06 06:43:06 +0200

    Add UAH to supported fiat currencies
    closes #6657

--- freqtrade/constants.py
+++ freqtrade/constants.py
@@ -86,8 +86,8 @@ SUPPORTED_FIAT = [
     "AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK",
     "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY",
     "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN",
-    "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR", "USD",
-    "BTC", "ETH", "XRP", "LTC", "BCH"
+    "RUB", "UAH", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR",
+    "USD", "BTC", "ETH", "XRP", "LTC", "BCH"
 ]
 
 MINIMAL_CONFIG = {
