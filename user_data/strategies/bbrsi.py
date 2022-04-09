3bd3adb97bbe122cc8277942632850b0fd72fb72
Bemhreth <bemhretgezahegn@gmail.com>
2022-04-09 13:08:42 +0300

    added waostrategy.py

--- user_data/strategies/bbrsi.py
+++ user_data/strategies/bbrsi.py
@@ -10,14 +10,13 @@ from pandas import DataFrame
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 import numpy  # noqa
-from datetime import datetime
-from wao.strategy_controller import StrategyController
+from wao.waostrategy import WAOStrategy
 
 
 class bbrsi(IStrategy):
-    controller = StrategyController('Freq_bbrsi')
     # Minimal ROI designed for the strategy.
     # This attribute will be overridden if the config file contains "minimal_roi"
+    waostrategy = WAOStrategy("bbrsi")
     minimal_roi = {
         "0": 0.07
     }
@@ -127,65 +126,3 @@ class bbrsi(IStrategy):
             'sell'] = 1
         return dataframe
 
-    def confirm_trade_entry(self, pair: str, order_type: str, amount: float, rate: float,
-                            time_in_force: str, current_time: datetime, **kwargs) -> bool:
-        """
-        Called right before placing a buy order.
-        Timing for this function is critical, so avoid doing heavy computations or
-        network requests in this method.
-
-        For full documentation please go to https://www.freqtrade.io/en/latest/strategy-advanced/
-
-        When not implemented by a strategy, returns True (always confirming).
-
-        :param pair: Pair that's about to be bought.
-        :param order_type: Order type (as configured in order_types). usually limit or market.
-        :param amount: Amount in target (quote) currency that's going to be traded.
-        :param rate: Rate that's going to be used when using limit orders
-        :param time_in_force: Time in force. Defaults to GTC (Good-til-cancelled).
-        :param current_time: datetime object, containing the current datetime
-        :param **kwargs: Ensure to keep this here so updates to this won't break your strategy.
-        :return bool: When True is returned, then the buy-order is placed on the exchange.
-            False aborts the process
-        """
-        # current_time = 2021-09-09 18:20:00+00:00
-
-        mode = "test"
-        coin = pair.split("/")[0]
-        brain = "Freq_" + self.__class__.__name__
-
-        self.controller.on_buy_signal(current_time, mode, coin, brain)
-        return True
-
-    def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
-                           rate: float, time_in_force: str, sell_reason: str,
-                           current_time: datetime, **kwargs) -> bool:
-        """
-        Called right before placing a regular sell order.
-        Timing for this function is critical, so avoid doing heavy computations or
-        network requests in this method.
-
-        For full documentation please go to https://www.freqtrade.io/en/latest/strategy-advanced/
-
-        When not implemented by a strategy, returns True (always confirming).
-
-        :param pair: Pair that's about to be sold.
-        :param order_type: Order type (as configured in order_types). usually limit or market.
-        :param amount: Amount in quote currency.
-        :param rate: Rate that's going to be used when using limit orders
-        :param time_in_force: Time in force. Defaults to GTC (Good-til-cancelled).
-        :param sell_reason: Sell reaso.
-            Can be any of ['roi', 'stop_loss', 'stoploss_on_exchange', 'trailing_stop_loss',
-                           'sell_signal', 'force_sell', 'emergency_sell']
-        :param current_time: datetime object, containing the current datetime
-        :param **kwargs: Ensure to keep this here so updates to this won't break your strategy.
-        :return bool: When True is returned, then the sell-order is placed on the exchange.
-            False aborts the process
-        """
-
-        coin = pair.split("/")[0]
-        brain = "Freq_" + self.__class__.__name__
-        mode = "test"
-
-        self.controller.on_sell_signal(sell_reason, current_time, mode, coin, brain)
-        return True
