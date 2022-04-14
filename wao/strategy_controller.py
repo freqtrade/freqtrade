1a33a0c35ba4401d0160afb2f62458552491b10b
teferi <teferi.aleme@gmail.com>
2022-04-14 10:15:19 -0700

    fix brain bug

--- wao/strategy_controller.py
+++ wao/strategy_controller.py
@@ -15,7 +15,7 @@ class StrategyController:
         self.brain = brain
         setup_429()
         if BrainConfig.IS_BACKTEST:
-            send_start_deliminator_message(brain, BrainConfig.BACKTEST_COIN,
+            send_start_deliminator_message(self.brain, BrainConfig.BACKTEST_COIN,
                                            BrainConfig.BACKTEST_MONTH_LIST[
                                                BrainConfig.BACKTEST_DATA_CLEANER_MONTH_INDEX],
                                            BrainConfig.BACKTEST_DATA_CLEANER_YEAR, BrainConfig.BACKTEST_DUP,
@@ -36,9 +36,9 @@ class StrategyController:
             if BrainConfig.IS_BACKTEST:
                 perform_back_test_sell(current_time)
             else:
-                perform_execute_sell(coin, brain, self.romeo_pool)
+                perform_execute_sell(coin, self.brain, self.romeo_pool)
 
-        self.__remove_from_pool(coin, brain)
+        self.__remove_from_pool(coin)
 
     def __buy_back_test(self, date_time, coin):
         time.sleep(BrainConfig.BACKTEST_THROTTLE_SECOND)
@@ -53,7 +53,7 @@ class StrategyController:
         else:
             perform_execute_buy(mode, coin, self.brain, self.romeo_pool)
 
-    def __remove_from_pool(self, coin, brain):
-        romeo = self.romeo_pool.get(coin + brain)
+    def __remove_from_pool(self, coin):
+        romeo = self.romeo_pool.get(coin + self.brain)
         if romeo is not None:
-            del self.romeo_pool[coin + brain]
+            del self.romeo_pool[coin + self.brain]
