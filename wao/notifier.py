a0ec253952dbcf548f557a44c203954fc4784522
teferi <teferi.aleme@gmail.com>
2022-04-24 21:25:24 -0700

    refactor

--- wao/notifier.py
+++ wao/notifier.py
@@ -18,7 +18,9 @@ def send_start_deliminator_message(brain, month, year):
 
 
 def post_request(text, is_from_429_watcher=False):
-    print("post_request: " + text)
+    if Config.TELEGRAM_LOG_ENABLED:
+        print("post_request: " + text + " ---------------------")
+
     if Config.NOTIFIER_ENABLED:
         telegram_bot_api_token = Config.NOTIFIER_TELEGRAM_BOT_API_TOKEN_429 if is_from_429_watcher else Config.NOTIFIER_TELEGRAM_BOT_API_TOKEN_BACKTEST
         result = requests.post('https://api.telegram.org/bot' + telegram_bot_api_token +
