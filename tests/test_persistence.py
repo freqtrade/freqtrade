575b4ead1a04bffc4c3064abcfe85fe77574b7c8
Matthias <xmatthias@outlook.com>
2022-06-17 06:29:17 +0000

    Update Test with funding_fee 0

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -884,6 +884,17 @@ def test_calc_close_trade_price(
         ('binance', False, 3, 2.2, 0.0025, 4.684999, 0.23366583, futures, -1),
         ('binance', True, 1, 2.2, 0.0025, -7.315, -0.12222222, futures, -1),
         ('binance', True, 3, 2.2, 0.0025, -7.315, -0.36666666, futures, -1),
+
+        # FUTURES, funding_fee=0
+        ('binance', False, 1, 2.1, 0.0025, 2.6925, 0.04476309, futures, 0),
+        ('binance', False, 3, 2.1, 0.0025, 2.6925, 0.13428928, futures, 0),
+        ('binance', True, 1, 2.1, 0.0025, -3.3074999, -0.05526316, futures, 0),
+        ('binance', True, 3, 2.1, 0.0025, -3.3074999, -0.16578947, futures, 0),
+
+        ('binance', False, 1, 1.9, 0.0025, -3.2925, -0.05473815, futures, 0),
+        ('binance', False, 3, 1.9, 0.0025, -3.2925, -0.16421446, futures, 0),
+        ('binance', True, 1, 1.9, 0.0025, 2.7075, 0.0452381, futures, 0),
+        ('binance', True, 3, 1.9, 0.0025, 2.7075, 0.13571429, futures, 0),
     ])
 @pytest.mark.usefixtures("init_persistence")
 def test_calc_profit(
