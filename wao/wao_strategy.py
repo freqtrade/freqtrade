e58091205d886f8d9a7814f9943aac77c68d09e7
Bemhreth <bemhretgezahegn@gmail.com>
2022-07-21 17:02:40 +0300

    get coin pairs

--- wao/wao_strategy.py
+++ wao/wao_strategy.py
@@ -17,6 +17,7 @@ class WAOStrategy(IStrategy):
 
     def __init__(self, config: dict, brain, time_out_hours, dup):
         super().__init__(config)
+        print("----------------------------------------------------" + str(config.get("pair_whitelist")))
         self.controller = WAOStrategyController(brain, time_out_hours, dup)
 
     def confirm_trade_entry(self, pair: str, order_type: str, amount: float, rate: float,
