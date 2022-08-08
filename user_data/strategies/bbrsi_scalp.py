765cbb05343f25839ced8da5c1068703517d2c98
teferi <teferi.aleme@gmail.com>
2022-08-07 20:01:25 -0700

    bbrsi strategy

--- user_data/strategies/bbrsi_scalp.py
+++ user_data/strategies/bbrsi_scalp.py
@@ -78,7 +78,7 @@ class bbrsi_scalp(WAOStrategy):
         # dataframe['slowk'] = stoch['slowk']
 
         # RSI
-        # dataframe['rsi'] = ta.RSI(dataframe)
+        dataframe['rsi'] = ta.RSI(dataframe)
 
         # Inverse Fisher transform on RSI, values [-1.0, 1.0] (https://goo.gl/2JGGoy)
         # rsi = 0.1 * (dataframe['rsi'] - 50)
@@ -106,9 +106,8 @@ class bbrsi_scalp(WAOStrategy):
         """
         dataframe.loc[
             (
-                    (dataframe['close'].shift(1) < dataframe['bb_lowerband'])
-                    & (dataframe['close'] > dataframe['bb_lowerband'])
-                    # & (dataframe['rsi'] < 50)
+                (qtpylib.crossed_above(dataframe['close'], dataframe['bb_lowerband'])) &
+                (dataframe['rsi'] < 50)
             ),
             'buy'] = 1
 
@@ -122,8 +121,8 @@ class bbrsi_scalp(WAOStrategy):
         """
         dataframe.loc[
             (
-                    (dataframe['close'] > dataframe['bb_upperband'])
-                    # | (dataframe['rsi'] > 60)
+                    (dataframe['close'] > dataframe['bb_upperband']) |
+                    (dataframe['rsi'] > 60)
             ),
             'sell'] = 1
         return dataframe
