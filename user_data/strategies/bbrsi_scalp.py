2dde7fed338ec6d3c49a73781d1f0e6dabf97f28
teferi <teferi.aleme@gmail.com>
2022-08-15 07:05:21 -0700

    minimal_roi

--- user_data/strategies/bbrsi_scalp.py
+++ user_data/strategies/bbrsi_scalp.py
@@ -19,8 +19,23 @@ class bbrsi_scalp(WAOStrategy):
 
     # Minimal ROI designed for the strategy.
     # This attribute will be overridden if the config file contains "minimal_roi"
+    # minimal_roi = {
+    #     "0": 0.07
+    # }
     minimal_roi = {
-        "0": 0.07
+        "240": 0.006,  # Exit after 500 minutes there is at least 0.5% profit
+        "220": 0.008,  # Exit after 500 minutes there is at least 0.5% profit
+        "200": 0.010,  # Exit after 40 minutes if there is at least 1% profit
+        "180": 0.011,  # Exit after 40 minutes if there is at least 1% profit
+        "160": 0.012,  # Exit after 40 minutes if there is at least 1% profit
+        "140": 0.013,  # Exit after 20 minutes if there is at least 1.5% profit
+        "120": 0.014,  # Exit after 20 minutes if there is at least 1.5% profit
+        "100": 0.015,  # Exit after 20 minutes if there is at least 1.5% profit
+        "80": 0.016,  # Exit after 20 minutes if there is at least 1.5% profit
+        "60": 0.017,  # Exit immediately if there is at least 2% profit
+        "40": 0.018,  # Exit immediately if there is at least 2% profit
+        "20": 0.019,  # Exit immediately if there is at least 2% profit
+        "0": 0.020,  # Exit immediately if there is at least 2% profit
     }
 
     # Optimal stoploss designed for the strategy
