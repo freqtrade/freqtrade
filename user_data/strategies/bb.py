c980f185c384ec71fc7905f13cf913f845400063
Bemhreth <bemhretgezahegn@gmail.com>
2022-09-05 17:59:12 +0300

    fix

--- user_data/strategies/bb.py
+++ user_data/strategies/bb.py
@@ -32,21 +32,21 @@ class bb(WAOStrategy):
     #     "0": 0.030,  # Exit immediately if there is at least 2% profit
     # }
 
-    # minimal_roi = {
-    #     "190": 0.005, # Exit after 500 minutes there is at least 0.5% profit
-    #     "175": 0.006, # Exit after 500 minutes there is at least 0.5% profit
-    #     "160": 0.007, # Exit after 40 minutes if there is at least 1% profit
-    #     "145": 0.008, # Exit after 40 minutes if there is at least 1% profit
-    #     "130": 0.009, # Exit after 40 minutes if there is at least 1% profit
-    #     "115": 0.010, # Exit after 20 minutes if there is at least 1.5% profit
-    #     "100": 0.011, # Exit after 20 minutes if there is at least 1.5% profit
-    #     "85": 0.012, # Exit after 20 minutes if there is at least 1.5% profit
-    #     "60": 0.013, # Exit after 20 minutes if there is at least 1.5% profit
-    #     "45": 0.014, # Exit immediately if there is at least 2% profit
-    #     "30": 0.016, # Exit immediately if there is at least 2% profit
-    #     "15": 0.018, # Exit immediately if there is at least 2% profit
-    #     "0": 0.020, # Exit immediately if there is at least 2% profit
-    # }
+    minimal_roi = {
+        "190": 0.005,  # Exit after 500 minutes there is at least 0.5% profit
+        "175": 0.006,  # Exit after 500 minutes there is at least 0.5% profit
+        "160": 0.007,  # Exit after 40 minutes if there is at least 1% profit
+        "145": 0.008,  # Exit after 40 minutes if there is at least 1% profit
+        "130": 0.009,  # Exit after 40 minutes if there is at least 1% profit
+        "115": 0.010,  # Exit after 20 minutes if there is at least 1.5% profit
+        "100": 0.011,  # Exit after 20 minutes if there is at least 1.5% profit
+        "85": 0.012,  # Exit after 20 minutes if there is at least 1.5% profit
+        "60": 0.013,  # Exit after 20 minutes if there is at least 1.5% profit
+        "45": 0.014,  # Exit immediately if there is at least 2% profit
+        "30": 0.016,  # Exit immediately if there is at least 2% profit
+        "15": 0.018,  # Exit immediately if there is at least 2% profit
+        "0": 0.020,  # Exit immediately if there is at least 2% profit
+    }
 
     # minimal_roi = {
     #     "180": 0.006,  # Exit after 500 minutes there is at least 0.5% profit
@@ -80,30 +80,14 @@ class bb(WAOStrategy):
     #     "0": 0.030,  # Exit immediately if there is at least 2% profit
     # }
 
-    minimal_roi = {
-        "190": 0.005,  # Exit after 500 minutes there is at least 0.5% profit
-        "175": 0.006,  # Exit after 500 minutes there is at least 0.5% profit
-        "160": 0.007,  # Exit after 40 minutes if there is at least 1% profit
-        "145": 0.008,  # Exit after 40 minutes if there is at least 1% profit
-        "130": 0.009,  # Exit after 40 minutes if there is at least 1% profit
-        "115": 0.010,  # Exit after 20 minutes if there is at least 1.5% profit
-        "100": 0.011,  # Exit after 20 minutes if there is at least 1.5% profit
-        "85": 0.012,  # Exit after 20 minutes if there is at least 1.5% profit
-        "60": 0.013,  # Exit after 20 minutes if there is at least 1.5% profit
-        "45": 0.014,  # Exit immediately if there is at least 2% profit
-        "30": 0.016,  # Exit immediately if there is at least 2% profit
-        "15": 0.018,  # Exit immediately if there is at least 2% profit
-        "0": 0.020,  # Exit immediately if there is at least 2% profit
-    }
-
     # Stoploss:
     stoploss = -0.01
 
     # Trailing stop:
-    trailing_stop = True
-    trailing_stop_positive = 0.281
-    trailing_stop_positive_offset = 0.299
-    trailing_only_offset_is_reached = True
+    trailing_stop = False
+    trailing_stop_positive = 0.089
+    trailing_stop_positive_offset = 0.11
+    trailing_only_offset_is_reached = False
 
     # run "populate_indicators" only for new candle
     process_only_new_candles = False
@@ -186,10 +170,10 @@ class bb(WAOStrategy):
         """
         dataframe.loc[
             (
-                   qtpylib.crossed_above(dataframe['close'], dataframe['bb_lowerband'])
-                    # & (dataframe['rsi'] < 50)
-                    # & (dataframe['ema_9'] > dataframe['sma_200'])
-                    # & (dataframe['macdhist'] > -0.08)
+                qtpylib.crossed_above(dataframe['close'], dataframe['bb_lowerband'])
+                # & (dataframe['rsi'] < 50)
+                # & (dataframe['ema_9'] > dataframe['sma_200'])
+                # & (dataframe['macdhist'] > -0.08)
             ),
             'buy'] = 1
 
@@ -203,9 +187,9 @@ class bb(WAOStrategy):
         """
         dataframe.loc[
             (
-                    (dataframe['close'] > dataframe['bb_upperband'])
-                    # | (dataframe['rsi'] > 60)
-                    # | (qtpylib.crossed_below(dataframe['ema_9'], dataframe['sma_200']))
+                (dataframe['close'] > dataframe['bb_upperband'])
+                # | (dataframe['rsi'] > 60)
+                # | (qtpylib.crossed_below(dataframe['ema_9'], dataframe['sma_200']))
 
             ),
             'sell'] = 1
--- user_data/strategies/bbrsi_optimized.py
+++ user_data/strategies/bbrsi_optimized.py
@@ -15,34 +15,37 @@ class bbrsi_optimized(WAOStrategy):
         self.coin = str(config.get('pairs')[0]).split('/')[0]
         if self.coin == 'BTC' or self.coin == 'ADA':
             self.brain = "Freq_bbrsi_scalp_ada_btc"
-        super().__init__(config, self.brain, 2, 0.15)
+        super().__init__(config, self.brain, 8, 0.15)
 
-    timeframe = '15m'
-
+    # Minimal ROI designed for the strategy.
+    # This attribute will be overridden if the config file contains "minimal_roi"
     minimal_roi = {
-        "240": 0.005,  # Exit after 500 minutes there is at least 0.5% profit
-        "220": 0.006,  # Exit after 500 minutes there is at least 0.5% profit
-        "200": 0.007,  # Exit after 40 minutes if there is at least 1% profit
-        "180": 0.008,  # Exit after 40 minutes if there is at least 1% profit
-        "160": 0.009,  # Exit after 40 minutes if there is at least 1% profit
-        "140": 0.010,  # Exit after 20 minutes if there is at least 1.5% profit
-        "120": 0.011,  # Exit after 20 minutes if there is at least 1.5% profit
-        "100": 0.012,  # Exit after 20 minutes if there is at least 1.5% profit
-        "80": 0.013,  # Exit after 20 minutes if there is at least 1.5% profit
-        "60": 0.014,  # Exit immediately if there is at least 2% profit
-        "40": 0.016,  # Exit immediately if there is at least 2% profit
-        "20": 0.018,  # Exit immediately if there is at least 2% profit
+        "190": 0.005,  # Exit after 500 minutes there is at least 0.5% profit
+        "175": 0.006,  # Exit after 500 minutes there is at least 0.5% profit
+        "160": 0.007,  # Exit after 40 minutes if there is at least 1% profit
+        "145": 0.008,  # Exit after 40 minutes if there is at least 1% profit
+        "130": 0.009,  # Exit after 40 minutes if there is at least 1% profit
+        "115": 0.010,  # Exit after 20 minutes if there is at least 1.5% profit
+        "100": 0.011,  # Exit after 20 minutes if there is at least 1.5% profit
+        "85": 0.012,  # Exit after 20 minutes if there is at least 1.5% profit
+        "60": 0.013,  # Exit after 20 minutes if there is at least 1.5% profit
+        "45": 0.014,  # Exit immediately if there is at least 2% profit
+        "30": 0.016,  # Exit immediately if there is at least 2% profit
+        "15": 0.018,  # Exit immediately if there is at least 2% profit
         "0": 0.020,  # Exit immediately if there is at least 2% profit
     }
 
-    # Stoploss:
+    # Optimal stoploss designed for the strategy
+    # This attribute will be overridden if the config file contains "stoploss"
     stoploss = -0.01
 
-    # Trailing stop:
+    # Optimal timeframe for the strategy
+    timeframe = '15m'
+
+    # trailing stoploss
     trailing_stop = False
-    trailing_stop_positive = 0.089
-    trailing_stop_positive_offset = 0.11
-    trailing_only_offset_is_reached = False
+    trailing_stop_positive = 0.01
+    trailing_stop_positive_offset = 0.02
 
     # run "populate_indicators" only for new candle
     process_only_new_candles = False
@@ -50,16 +53,7 @@ class bbrsi_optimized(WAOStrategy):
     # Experimental settings (configuration will overide these if set)
     use_sell_signal = True
     sell_profit_only = True
-    ignore_roi_if_buy_signal = True
-
-    # Optional order type mapping.
-    order_types = {
-        'buy': 'limit',
-        'sell': 'limit',
-        'trailing_stop_loss': 'limit',
-        'stoploss': 'limit',
-        'stoploss_on_exchange': False
-    }
+    ignore_roi_if_buy_signal = False
 
     def informative_pairs(self):
         """
@@ -77,9 +71,8 @@ class bbrsi_optimized(WAOStrategy):
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         """
         Adds several different TA indicators to the given DataFrame
-
         Performance Note: For the best performance be frugal on the number of indicators
-        you are using. Let uncomment only the indicator you are using in your refined-strategies
+        you are using. Let uncomment only the indicator you are using in your strategies
         or your hyperopt configuration, otherwise you will waste your memory and CPU usage.
         """
 
@@ -88,17 +81,7 @@ class bbrsi_optimized(WAOStrategy):
         # dataframe['slowk'] = stoch['slowk']
 
         # RSI
-        # dataframe['rsi'] = ta.RSI(dataframe)
-        # SMA
-        # dataframe['sma_200'] = ta.SMA(dataframe, timeperiod=200)
-        # EMA
-        # dataframe['ema_9'] = ta.EMA(dataframe, timeperiod=9)
-
-        # MACD
-        # macd = ta.MACD(dataframe)
-        # dataframe['macd'] = macd['macd']
-        # dataframe['macdsignal'] = macd['macdsignal']
-        # dataframe['macdhist'] = macd['macdhist']
+        dataframe['rsi'] = ta.RSI(dataframe)
 
         # Inverse Fisher transform on RSI, values [-1.0, 1.0] (https://goo.gl/2JGGoy)
         # rsi = 0.1 * (dataframe['rsi'] - 50)
@@ -126,10 +109,8 @@ class bbrsi_optimized(WAOStrategy):
         """
         dataframe.loc[
             (
-                qtpylib.crossed_above(dataframe['close'], dataframe['bb_lowerband'])
-                # & (dataframe['rsi'] < 50)
-                # & (dataframe['ema_9'] > dataframe['sma_200'])
-                # & (dataframe['macdhist'] > -0.08)
+                    (qtpylib.crossed_above(dataframe['close'], dataframe['bb_lowerband'])) &
+                    (dataframe['rsi'] < 50)
             ),
             'buy'] = 1
 
@@ -143,10 +124,8 @@ class bbrsi_optimized(WAOStrategy):
         """
         dataframe.loc[
             (
-                (dataframe['close'] > dataframe['bb_upperband'])
-                # | (dataframe['rsi'] > 60)
-                # | (qtpylib.crossed_below(dataframe['ema_9'], dataframe['sma_200']))
-
+                    (dataframe['close'] > dataframe['bb_upperband']) |
+                    (dataframe['rsi'] > 60)
             ),
             'sell'] = 1
-        return dataframe
+        return
