a9fd12b816d5a36a8233f508dec92d3f4bb4ab40
Matthias <xmatthias@outlook.com>
2022-09-07 18:28:04 +0200

    Allow more dynamic timeframes by disabling "choice"
    part of #7366

--- freqtrade/commands/cli_options.py
+++ freqtrade/commands/cli_options.py
@@ -455,8 +455,6 @@ AVAILABLE_CLI_OPTIONS = {
         '-t', '--timeframes',
         help='Specify which tickers to download. Space-separated list. '
         'Default: `1m 5m`.',
-        choices=['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h',
-                 '6h', '8h', '12h', '1d', '3d', '1w', '2w', '1M', '1y'],
         default=['1m', '5m'],
         nargs='+',
     ),
