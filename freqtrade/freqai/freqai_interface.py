42c75b4a7beb71603cad305f72154410209b791e
robcaulk <rob.caulk@gmail.com>
2022-09-19 19:16:32 +0200

    combine log messages

--- freqtrade/freqai/freqai_interface.py
+++ freqtrade/freqai/freqai_interface.py
@@ -702,8 +702,8 @@ class IFreqaiModel(ABC):
         """
         current_pairlist = self.config.get("exchange", {}).get("pair_whitelist")
         if not self.dd.pair_dict:
-            logger.info('Set fresh train queue from whitelist.')
-            logger.info(f'Queue: {current_pairlist}')
+            logger.info('Set fresh train queue from whitelist. '
+                        f'Queue: {current_pairlist}')
             return deque(current_pairlist)
 
         best_queue = deque()
@@ -717,8 +717,8 @@ class IFreqaiModel(ABC):
             if pair not in best_queue:
                 best_queue.appendleft(pair)
 
-        logger.info('Set existing queue from trained timestamps.')
-        logger.info(f'Best approximation queue: {best_queue}')
+        logger.info('Set existing queue from trained timestamps. '
+                    f'Best approximation queue: {best_queue}')
         return best_queue
 
     # Following methods which are overridden by user made prediction models.
