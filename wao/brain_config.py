4811bac52da520cdf95fe009f2bd07772d4a05c6
Bemhreth <bemhretgezahegn@gmail.com>
2022-10-01 22:03:56 +0300

    changes made

--- wao/brain_config.py
+++ wao/brain_config.py
@@ -16,8 +16,8 @@ class BrainConfig:
     BACKTEST_SIGNAL_LIST = []
     BACKTEST_THROTTLE_SECOND = 1
     MODE = "test"  # test or prod
-    CUMULATIVE_PROFIT_FILE_PATH = EXECUTION_PATH + "/_cumulative_profit.txt"
-    CUMULATIVE_PROFIT_BINANCE_FILE_PATH = EXECUTION_PATH + "/_cumulative_profit_binance.txt"
-    INITIAL_ACCOUNT_BALANCE_BINANCE_FILE_PATH = EXECUTION_PATH + "/_initial_account_balance_binance.txt"
+    CUMULATIVE_PROFIT_FILE_PATH = FREQTRADE_PATH + "/_cumulative_profit.txt"
+    CUMULATIVE_PROFIT_BINANCE_FILE_PATH = FREQTRADE_PATH + "/_cumulative_profit_binance.txt"
+    INITIAL_ACCOUNT_BALANCE_BINANCE_FILE_PATH = FREQTRADE_PATH + "/_initial_account_balance_binance.txt"
     IS_SMOOTH_ERROR_HANDLING_ENABLED = True
 
