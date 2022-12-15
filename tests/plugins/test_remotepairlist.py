cd1b8b9cee37a0ac412a57af51f02348b40d9565
Bloodhunter4rc <ouwtou@hotmail.com>
2022-12-15 18:14:37 +0100

    single space removed for the unit test to pass..

--- tests/plugins/test_remotepairlist.py
+++ tests/plugins/test_remotepairlist.py
@@ -108,7 +108,7 @@ def test_fetch_pairlist_timeout_keep_last_pairlist(mocker, rpl_config, caplog):
     remote_pairlist._last_pairlist = ["BTC/USDT", "ETH/USDT", "LTC/USDT"]
 
     pairs, time_elapsed, info = remote_pairlist.fetch_pairlist()
-    assert log_has(f"Was not able to fetch pairlist from: {remote_pairlist._pairlist_url} ", caplog)
+    assert log_has(f"Was not able to fetch pairlist from: {remote_pairlist._pairlist_url}", caplog)
     assert log_has("Keeping last fetched pairlist", caplog)
     assert pairs == ["BTC/USDT", "ETH/USDT", "LTC/USDT"]
 
