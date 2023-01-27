fa033965c8b032d5cbc56821b42e968b8987da47
Matthias <xmatthias@outlook.com>
2023-01-27 19:34:29 +0100

    use "swap" for bybit

--- freqtrade/exchange/bybit.py
+++ freqtrade/exchange/bybit.py
@@ -28,7 +28,6 @@ class Bybit(Exchange):
 
     _ft_has: Dict = {
         "ohlcv_candle_limit": 1000,
-        "ccxt_futures_name": "linear",
         "ohlcv_has_history": False,
     }
     _ft_has_futures: Dict = {
