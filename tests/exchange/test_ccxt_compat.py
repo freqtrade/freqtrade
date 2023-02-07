e2d81b0ce0afd6e3c2ed007082d7ac70c27a3ed0
Matthias <xmatthias@outlook.com>
2023-02-07 19:45:37 +0100

    Skip binanceus ccxt test

--- tests/exchange/test_ccxt_compat.py
+++ tests/exchange/test_ccxt_compat.py
@@ -468,9 +468,13 @@ class TestCCXTExchange():
 
     def test_ccxt__async_get_candle_history(self, exchange: EXCHANGE_FIXTURE_TYPE):
         exc, exchangename = exchange
-        # For some weired reason, this test returns random lengths for bittrex.
-        if not exc._ft_has['ohlcv_has_history'] or exchangename in ('bittrex'):
-            return
+        if exchangename in ('binanceus', 'bittrex'):
+            # TODO: reenable binanceus test once downtime "ages out" (2023-02-06)
+            # For some weired reason, this test returns random lengths for bittrex.
+            pytest.skip("Exchange doesn't provide stable ohlcv history")
+
+        if not exc._ft_has['ohlcv_has_history']:
+            pytest.skip("Exchange does not support candle history")
         pair = EXCHANGES[exchangename]['pair']
         timeframe = EXCHANGES[exchangename]['timeframe']
         self.ccxt__async_get_candle_history(
