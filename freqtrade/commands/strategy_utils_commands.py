a3988f56b28ddef95fd425e9804d3154491ad994
hippocritical <gunngirspam@gmail.com>
2023-03-10 09:23:56 +0100

    Sorry matthias, did not see that you already committed something and did overwrite you. Added your version to it instead of mine and pushed again (since it was already overwritten by me).

--- freqtrade/commands/strategy_utils_commands.py
+++ freqtrade/commands/strategy_utils_commands.py
@@ -20,34 +20,31 @@ def start_strategy_update(args: Dict[str, Any]) -> None:
     :return: None
     """
 
-    if sys.version_info <= (3, 8):
-        print("This code requires Python 3.9 or higher. "
-              "We cannot continue. "
-              "Please upgrade your python version to use this command.")
+    if sys.version_info == (3, 8):  # pragma: no cover
+        sys.exit("Freqtrade strategy updater requires Python version >= 3.9")
 
-    else:
-        config = setup_utils_configuration(args, RunMode.UTIL_NO_EXCHANGE)
+    config = setup_utils_configuration(args, RunMode.UTIL_NO_EXCHANGE)
 
-        strategy_objs = StrategyResolver.search_all_objects(
-            config, enum_failed=False, recursive=config.get('recursive_strategy_search', False))
+    strategy_objs = StrategyResolver.search_all_objects(
+        config, enum_failed=False, recursive=config.get('recursive_strategy_search', False))
 
-        filtered_strategy_objs = []
-        if 'strategy_list' in args:
-            for args_strategy in args['strategy_list']:
-                for strategy_obj in strategy_objs:
-                    if (strategy_obj['name'] == args_strategy
-                            and strategy_obj not in filtered_strategy_objs):
-                        filtered_strategy_objs.append(strategy_obj)
-                        break
-
-            for filtered_strategy_obj in filtered_strategy_objs:
-                start_conversion(filtered_strategy_obj, config)
-        else:
-            processed_locations = set()
+    filtered_strategy_objs = []
+    if 'strategy_list' in args:
+        for args_strategy in args['strategy_list']:
             for strategy_obj in strategy_objs:
-                if strategy_obj['location'] not in processed_locations:
-                    processed_locations.add(strategy_obj['location'])
-                    start_conversion(strategy_obj, config)
+                if (strategy_obj['name'] == args_strategy
+                        and strategy_obj not in filtered_strategy_objs):
+                    filtered_strategy_objs.append(strategy_obj)
+                    break
+
+        for filtered_strategy_obj in filtered_strategy_objs:
+            start_conversion(filtered_strategy_obj, config)
+    else:
+        processed_locations = set()
+        for strategy_obj in strategy_objs:
+            if strategy_obj['location'] not in processed_locations:
+                processed_locations.add(strategy_obj['location'])
+                start_conversion(strategy_obj, config)
 
 
 def start_conversion(strategy_obj, config):
