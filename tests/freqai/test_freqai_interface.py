2a1a8c0e644b39b72b5a88eb78945162274d8bde
Yinon Polak <inon51@gmail.com>
2023-03-19 14:49:12 +0200

    fix test

--- tests/freqai/test_freqai_interface.py
+++ tests/freqai/test_freqai_interface.py
@@ -85,22 +85,6 @@ def test_extract_data_and_train_model_Standard(mocker, freqai_conf, model, pca,
     if 'test_3ac' in model or 'test_4ac' in model:
         freqai_conf["freqaimodel_path"] = str(Path(__file__).parents[1] / "freqai" / "test_models")
 
-    if 'PyTorchClassifierMultiTarget' in model:
-        model_save_ext = 'zip'
-        freqai_conf['freqai']['model_training_parameters'].update({
-            "learning_rate": 3e-4,
-            "trainer_kwargs": {
-                "max_iters": 1,
-                "batch_size": 64,
-                "max_n_eval_batches": 1,
-            },
-            "model_kwargs": {
-                "hidden_dim": 32,
-                "dropout_percent": 0.2,
-                "n_layer": 1,
-            }
-        })
-
     strategy = get_patched_freqai_strategy(mocker, freqai_conf)
     exchange = get_patched_exchange(mocker, freqai_conf)
     strategy.dp = DataProvider(freqai_conf, exchange)
@@ -184,7 +168,7 @@ def test_extract_data_and_train_model_MultiTargets(mocker, freqai_conf, model, s
     'CatboostClassifier',
     'XGBoostClassifier',
     'XGBoostRFClassifier',
-    'PyTorchClassifierMultiTarget',
+    'PyTorchClassifier',
     ])
 def test_extract_data_and_train_model_Classifiers(mocker, freqai_conf, model):
     if (is_arm() or is_py11()) and model == 'CatboostClassifier':
