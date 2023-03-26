1c9abd9e3579097bb6c3c323e2d765afe551f351
Matthias <xmatthias@outlook.com>
2023-03-26 17:27:52 +0200

    Properly respect can_short flag in backtesting
    closes  #8387

--- freqtrade/optimize/backtesting.py
+++ freqtrade/optimize/backtesting.py
@@ -203,6 +203,8 @@ class Backtesting:
         # since a "perfect" stoploss-exit is assumed anyway
         # And the regular "stoploss" function would not apply to that case
         self.strategy.order_types['stoploss_on_exchange'] = False
+        # Update can_short flag
+        self._can_short = self.trading_mode != TradingMode.SPOT and strategy.can_short
 
         self.strategy.ft_bot_start()
 
