b6aac5079beb3f4f06320664665426406f7a1ba1
Matthias <xmatthias@outlook.com>
2023-04-09 16:21:30 +0200

    REmove Rich-progress wrapper again

--- freqtrade/util/__init__.py
+++ freqtrade/util/__init__.py
@@ -1,3 +1,2 @@
 from freqtrade.util.ft_precise import FtPrecise  # noqa: F401
 from freqtrade.util.periodic_cache import PeriodicCache  # noqa: F401
-from freqtrade.util.rich_progress import FtProgress  # noqa: F401
