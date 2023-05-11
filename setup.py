6000e68420810ac239faf2b243587dd08d740da7
Matthias <xmatthias@outlook.com>
2023-05-11 20:51:33 +0200

    bump ccxt min dependency

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     ],
     install_requires=[
         # from requirements.txt
-        'ccxt>=2.6.26',
+        'ccxt>=3.0.0',
         'SQLAlchemy>=2.0.6',
         'python-telegram-bot>=20.1',
         'arrow>=0.17.0',
