4d4589becded66667ae91ff2b91f8dc6712fa81a
robcaulk <rob.caulk@gmail.com>
2023-06-07 14:00:00 +0200

    fix isort in tests

--- tests/freqai/test_freqai_datakitchen.py
+++ tests/freqai/test_freqai_datakitchen.py
@@ -10,8 +10,8 @@ from freqtrade.data.dataprovider import DataProvider
 from freqtrade.exceptions import OperationalException
 from freqtrade.freqai.data_kitchen import FreqaiDataKitchen
 from tests.conftest import get_patched_exchange
-from tests.freqai.conftest import make_unfiltered_dataframe
-from tests.freqai.conftest import get_patched_data_kitchen, get_patched_freqai_strategy
+from tests.freqai.conftest import (get_patched_data_kitchen, get_patched_freqai_strategy,
+                                   make_unfiltered_dataframe)
 from tests.freqai.test_freqai_interface import is_mac
 
 
