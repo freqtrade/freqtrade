7453ff2fb587938bd7871f2f4c89cd7edfe04ebb
Matthias <xmatthias@outlook.com>
2023-06-17 15:13:56 +0200

    Migrate download-data out of commands section

--- freqtrade/data/history/__init__.py
+++ freqtrade/data/history/__init__.py
@@ -6,7 +6,7 @@ Includes:
 * download data from exchange and store to disk
 """
 # flake8: noqa: F401
-from .history_utils import (convert_trades_to_ohlcv, get_timerange, load_data, load_pair_history,
-                            refresh_backtest_ohlcv_data, refresh_backtest_trades_data, refresh_data,
-                            validate_backtest_data)
+from .history_utils import (convert_trades_to_ohlcv, download_data_main, get_timerange, load_data,
+                            load_pair_history, refresh_backtest_ohlcv_data,
+                            refresh_backtest_trades_data, refresh_data, validate_backtest_data)
 from .idatahandler import get_datahandler
