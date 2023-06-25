5e084ad2e5701eea5d007643d48926ab3ed0e207
Matthias <xmatthias@outlook.com>
2023-06-25 16:59:53 +0200

    convert optimize_reports to a package

--- tests/optimize/test_backtesting.py
+++ tests/optimize/test_backtesting.py
@@ -1437,7 +1437,7 @@ def test_backtest_start_multi_strat(default_conf, mocker, caplog, testdatadir):
     strattable_mock = MagicMock()
     strat_summary = MagicMock()
 
-    mocker.patch.multiple('freqtrade.optimize.optimize_reports',
+    mocker.patch.multiple('freqtrade.optimize.optimize_reports.optimize_reports',
                           text_table_bt_results=text_table_mock,
                           text_table_strategy=strattable_mock,
                           generate_pair_metrics=MagicMock(),
