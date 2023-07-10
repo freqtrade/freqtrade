1dbc294b80327a9b7c2e99f7a248e15cfd492582
Matthias <xmatthias@outlook.com>
2023-07-10 07:11:29 +0200

    Improve order __REPR__ with date

--- freqtrade/persistence/trade_model.py
+++ freqtrade/persistence/trade_model.py
@@ -121,9 +121,9 @@ class Order(ModelBase):
 
     def __repr__(self):
 
-        return (f"Order(id={self.id}, order_id={self.order_id}, trade_id={self.ft_trade_id}, "
+        return (f"Order(id={self.id}, trade={self.ft_trade_id}, order_id={self.order_id}, "
                 f"side={self.side}, filled={self.safe_filled}, price={self.safe_price}, "
-                f"order_type={self.order_type}, status={self.status})")
+                f"status={self.status}, date={self.order_date:{DATETIME_PRINT_FORMAT}})")
 
     def update_from_ccxt_object(self, order):
         """
