5ee4586989b4011f4237a614457257af6029b200
Matthias <xmatthias@outlook.com>
2023-07-18 21:14:55 +0200

    use find-links for windows install

--- build_helpers/install_windows.ps1
+++ build_helpers/install_windows.ps1
@@ -5,7 +5,7 @@ python -m pip install --upgrade pip wheel
 $pyv = python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')"
 
 
-pip install --user build_helpers\TA_Lib-*.whl
+pip install --find-links=build_helpers\ TA-Lib
 
 pip install -r requirements-dev.txt
 pip install -e .
