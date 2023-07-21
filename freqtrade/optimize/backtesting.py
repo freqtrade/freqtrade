9bfe96d4d61cb191fd45ee5c92a63716874d1fc6
Matthias <xmatthias@outlook.com>
2023-07-21 20:27:52 +0200

    Simplify advise calls by extracting that part into a method.

--- freqtrade/optimize/backtesting.py
+++ freqtrade/optimize/backtesting.py
@@ -367,11 +367,7 @@ class Backtesting:
             if not pair_data.empty:
                 # Cleanup from prior runs
                 pair_data.drop(HEADERS[5:] + ['buy', 'sell'], axis=1, errors='ignore')
-
-            df_analyzed = self.strategy.advise_exit(
-                self.strategy.advise_entry(pair_data, {'pair': pair}),
-                {'pair': pair}
-            ).copy()
+            df_analyzed = self.strategy.ft_advise_signals(pair_data, {'pair': pair})
             # Trim startup period from analyzed dataframe
             df_analyzed = processed[pair] = pair_data = trim_dataframe(
                 df_analyzed, self.timerange, startup_candles=self.required_startup)
