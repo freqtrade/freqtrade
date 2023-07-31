5c68b0d38e5f5b2046bb5b7edadbc9fd2220da44
Matthias <xmatthias@outlook.com>
2023-07-31 20:22:55 +0200

    Add BacktestMetadataType

--- freqtrade/types/backtest_result_type.py
+++ freqtrade/types/backtest_result_type.py
@@ -3,8 +3,13 @@ from typing import Any, Dict, List
 from typing_extensions import TypedDict
 
 
+class BacktestMetadataType(TypedDict):
+    run_id: str
+    backtest_start_time: int
+
+
 class BacktestResultType(TypedDict):
-    metadata: Dict[str, Any]
+    metadata: Dict[str, Any]  # BacktestMetadataType
     strategy: Dict[str, Any]
     strategy_comparison: List[Any]
 
