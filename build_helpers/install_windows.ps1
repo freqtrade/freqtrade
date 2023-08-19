f5e0543113cb99a34ce9932c5b7393bbd32c6a57
Matthias <xmatthias@outlook.com>
2023-08-19 20:50:55 +0200

    Fix windows CI failure

--- build_helpers/install_windows.ps1
+++ build_helpers/install_windows.ps1
@@ -5,7 +5,7 @@ python -m pip install --upgrade pip wheel
 $pyv = python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')"
 
 
-pip install --find-links=build_helpers\ TA-Lib
+pip install --find-links=build_helpers\ TA-Lib==0.4.27
 
 pip install -r requirements-dev.txt
 pip install -e .
