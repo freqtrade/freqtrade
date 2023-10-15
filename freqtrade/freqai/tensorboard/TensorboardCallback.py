1e1b8dbe538bba830b6903e1e4b88e63ca8eb6ee
Matthias <xmatthias@outlook.com>
2023-10-15 11:52:18 +0200

    Handle multiproc calls for now

--- freqtrade/freqai/tensorboard/TensorboardCallback.py
+++ freqtrade/freqai/tensorboard/TensorboardCallback.py
@@ -50,9 +50,12 @@ class TensorboardCallback(BaseCallback):
         if self.training_env is None:
             return True
 
-        tensorboard_metrics = (
-            self.training_env.envs[0].unwrapped.tensorboard_metrics  # type: ignore[attr-defined]
-        )
+        if hasattr(self.training_env, 'envs'):
+            tensorboard_metrics = self.training_env.envs[0].unwrapped.tensorboard_metrics
+
+        else:
+            # For RL-multiproc - usage of [0] might need to be evaluated
+            tensorboard_metrics = self.training_env.get_attr("tensorboard_metrics")[0]
 
         for metric in local_info:
             if metric not in ["episode", "terminal_observation"]:
