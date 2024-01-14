29d2fc2e1bcc01070e7d62713424b8a2b68d1db9
Matthias <xmatthias@outlook.com>
2024-01-14 15:29:09 +0100

    Skip freqAI tests on py3.12

--- tests/freqai/test_freqai_interface.py
+++ tests/freqai/test_freqai_interface.py
@@ -25,10 +25,14 @@ def is_arm() -> bool:
 
 
 def can_run_model(model: str) -> None:
+    is_pytorch_model = 'Reinforcement' in model or 'PyTorch' in model
+
+    if is_py12() and ("Catboost" in model or is_pytorch_model):
+        pytest.skip("Model not supported on python 3.12 yet.")
+
     if is_arm() and "Catboost" in model:
         pytest.skip("CatBoost is not supported on ARM.")
 
-    is_pytorch_model = 'Reinforcement' in model or 'PyTorch' in model
     if is_pytorch_model and is_mac() and not is_arm():
         pytest.skip("Reinforcement learning / PyTorch module not available on intel based Mac OS.")
 
