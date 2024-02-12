bff93e31c8bfe16a99d2c2d7d1d186adf86361be
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-02-12 13:20:52 +0100

    Moves orderflow logic to it's own file

--- freqtrade/data/converter/__init__.py
+++ freqtrade/data/converter/__init__.py
@@ -1,9 +1,8 @@
 from freqtrade.data.converter.converter import (clean_ohlcv_dataframe, convert_ohlcv_format,
                                                 ohlcv_fill_up_missing_data, ohlcv_to_dataframe,
-                                                order_book_to_dataframe,
-                                                populate_dataframe_with_trades,
-                                                reduce_dataframe_footprint, trim_dataframe,
-                                                trim_dataframes)
+                                                order_book_to_dataframe, reduce_dataframe_footprint,
+                                                trim_dataframe, trim_dataframes)
+from freqtrade.data.converter.orderflow import populate_dataframe_with_trades
 from freqtrade.data.converter.trade_converter import (convert_trades_format,
                                                       convert_trades_to_ohlcv, trades_convert_types,
                                                       trades_df_remove_duplicates,
