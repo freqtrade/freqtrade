c0da1b6922891afc66b3071237ce3220f1421cdd
Matthias <xmatthias@outlook.com>
2024-02-16 20:04:49 +0100

    Fix edge-case when calculating cagr
    edge-case with leveraged trades - yielding a negative final balance.
    closes #9820

--- freqtrade/data/metrics.py
+++ freqtrade/data/metrics.py
@@ -191,6 +191,9 @@ def calculate_cagr(days_passed: int, starting_balance: float, final_balance: flo
     :param final_balance: Final balance to calculate CAGR against
     :return: CAGR
     """
+    if final_balance < 0:
+        # With leveraged trades, final_balance can become negative.
+        return 0
     return (final_balance / starting_balance) ** (1 / (days_passed / 365)) - 1
 
 
