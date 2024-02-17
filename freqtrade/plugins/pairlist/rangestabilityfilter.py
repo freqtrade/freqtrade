78d8a4df2ea41cab6e2c9932091267f1d9316518
Matthias <xmatthias@outlook.com>
2024-02-17 16:29:52 +0100

    Use "ohlcv_with_cache" for further pairlists

--- freqtrade/plugins/pairlist/rangestabilityfilter.py
+++ freqtrade/plugins/pairlist/rangestabilityfilter.py
@@ -101,11 +101,7 @@ class RangeStabilityFilter(IPairList):
             (p, '1d', self._def_candletype) for p in pairlist if p not in self._pair_cache]
 
         since_ms = dt_ts(dt_floor_day(dt_now()) - timedelta(days=self._days - 1))
-        # Get all candles
-        candles = {}
-        if needed_pairs:
-            candles = self._exchange.refresh_latest_ohlcv(needed_pairs, since_ms=since_ms,
-                                                          cache=False)
+        candles = self._exchange.refresh_ohlcv_with_cache(needed_pairs, since_ms=since_ms)
 
         if self._enabled:
             for p in deepcopy(pairlist):
