3680e199ce56672ed36e0b599deb6ba189861953
Matthias <xmatthias@outlook.com>
2024-02-22 06:15:26 +0100

    Fix range-stability filter downloading too little data

--- freqtrade/plugins/pairlist/rangestabilityfilter.py
+++ freqtrade/plugins/pairlist/rangestabilityfilter.py
@@ -100,7 +100,7 @@ class RangeStabilityFilter(IPairList):
         needed_pairs: ListPairsWithTimeframes = [
             (p, '1d', self._def_candletype) for p in pairlist if p not in self._pair_cache]
 
-        since_ms = dt_ts(dt_floor_day(dt_now()) - timedelta(days=self._days - 1))
+        since_ms = dt_ts(dt_floor_day(dt_now()) - timedelta(days=self._days + 1))
         candles = self._exchange.refresh_ohlcv_with_cache(needed_pairs, since_ms=since_ms)
 
         if self._enabled:
