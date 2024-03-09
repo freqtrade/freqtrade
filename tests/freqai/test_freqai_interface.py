cb1f49e81ce142f830f5b0dd0f359a0171fea4b0
Matthias <xmatthias@outlook.com>
2024-03-09 19:25:17 +0100

    Don't run torch models on 3.12 yet

--- tests/freqai/test_freqai_interface.py
+++ tests/freqai/test_freqai_interface.py
@@ -27,6 +27,9 @@ def is_arm() -> bool:
 def can_run_model(model: str) -> None:
     is_pytorch_model = 'Reinforcement' in model or 'PyTorch' in model
 
+    if is_py12() and is_pytorch_model:
+        pytest.skip("Model not supported on python 3.12 yet.")
+
     if is_arm() and "Catboost" in model:
         pytest.skip("CatBoost is not supported on ARM.")
 
