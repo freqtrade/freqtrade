f0a25ea4858f66db5c440edefdc04ba815c28a4b
Matthias <xmatthias@outlook.com>
2024-04-09 20:32:15 +0200

    feat: Add __all__ export to strategy's init file

--- freqtrade/strategy/__init__.py
+++ freqtrade/strategy/__init__.py
@@ -1,4 +1,6 @@
 # flake8: noqa: F401
+from typing import Dict, List, Optional, Union
+
 from freqtrade.exchange import (
     timeframe_to_minutes,
     timeframe_to_msecs,
@@ -6,6 +8,7 @@ from freqtrade.exchange import (
     timeframe_to_prev_date,
     timeframe_to_seconds,
 )
+from freqtrade.persistence import Order, PairLocks, Trade
 from freqtrade.strategy.informative_decorator import informative
 from freqtrade.strategy.interface import IStrategy
 from freqtrade.strategy.parameters import (
@@ -20,3 +23,30 @@ from freqtrade.strategy.strategy_helper import (
     stoploss_from_absolute,
     stoploss_from_open,
 )
+
+
+__all__ = [
+    "timeframe_to_minutes",
+    "timeframe_to_next_date",
+    "timeframe_to_prev_date",
+    "informative",
+    "IStrategy",
+    "Trade",
+    "Order",
+    "PairLocks",
+    # Parameters
+    "BooleanParameter",
+    "CategoricalParameter",
+    "DecimalParameter",
+    "IntParameter",
+    "RealParameter",
+    # Strategy helper functions
+    "merge_informative_pair",
+    "stoploss_from_absolute",
+    "stoploss_from_open",
+    # Typings
+    "List",
+    "Optional",
+    "Union",
+    "Dict",
+]
