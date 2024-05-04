05215ebe9db0ff31e4adcef2b62839f2f7daee5e
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2024-05-04 11:55:05 +0900

    correctly close trade that fully exited through adjust trade

--- freqtrade/optimize/backtesting.py
+++ freqtrade/optimize/backtesting.py
@@ -1199,6 +1199,17 @@ class Backtesting:
                     LocalTrade.close_bt_trade(trade)
                 self.wallets.update()
                 self.run_protections(pair, current_time, trade.trade_direction)
+            else:
+                closed_exit_order = trade.select_order(trade.exit_side, is_open=False)
+                if closed_exit_order:
+                    # check for full exit from adjust trade position
+                    remaining = trade.amount - closed_exit_order.safe_filled
+                    if remaining == 0:
+                        trade.close_date = current_time
+                        trade.close(closed_exit_order.ft_price, show_msg=False)
+                        LocalTrade.close_bt_trade(trade)
+                        self.wallets.update()
+                        self.run_protections(pair, current_time, trade.trade_direction)
         return open_trade_count_start
 
     def backtest(self, processed: Dict,
