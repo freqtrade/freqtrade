208797452053c41d1e9549ccae7d4bffd839beb2
Matthias <xmatthias@outlook.com>
2024-06-08 09:40:32 +0200

    Fix some direct usages of "/tmp"

--- tests/test_directory_operations.py
+++ tests/test_directory_operations.py
@@ -24,16 +24,16 @@ def test_create_datadir(mocker, default_conf, caplog) -> None:
     assert log_has("Created data directory: /foo/bar", caplog)
 
 
-def test_create_userdata_dir(mocker, default_conf, caplog) -> None:
+def test_create_userdata_dir(mocker, tmp_path, caplog) -> None:
     mocker.patch.object(Path, "is_dir", MagicMock(return_value=False))
     md = mocker.patch.object(Path, "mkdir", MagicMock())
 
-    x = create_userdata_dir("/tmp/bar", create_dir=True)
+    x = create_userdata_dir(f"{tmp_path}/bar", create_dir=True)
     assert md.call_count == 10
     assert md.call_args[1]["parents"] is False
-    assert log_has(f'Created user-data directory: {Path("/tmp/bar")}', caplog)
+    assert log_has(f'Created user-data directory: {f"{tmp_path}/bar"}', caplog)
     assert isinstance(x, Path)
-    assert str(x) == str(Path("/tmp/bar"))
+    assert str(x) == f"{tmp_path}/bar"
 
 
 def test_create_userdata_dir_and_chown(mocker, tmp_path, caplog) -> None:
@@ -54,11 +54,11 @@ def test_create_userdata_dir_and_chown(mocker, tmp_path, caplog) -> None:
     del os.environ["FT_APP_ENV"]
 
 
-def test_create_userdata_dir_exists(mocker, default_conf, caplog) -> None:
+def test_create_userdata_dir_exists(mocker, tmp_path) -> None:
     mocker.patch.object(Path, "is_dir", MagicMock(return_value=True))
     md = mocker.patch.object(Path, "mkdir", MagicMock())
 
-    create_userdata_dir("/tmp/bar")
+    create_userdata_dir(f"{tmp_path}/bar")
     assert md.call_count == 0
 
 
@@ -73,21 +73,19 @@ def test_create_userdata_dir_exists_exception(mocker, default_conf, caplog) -> N
     assert md.call_count == 0
 
 
-def test_copy_sample_files(mocker, default_conf, caplog) -> None:
+def test_copy_sample_files(mocker, tmp_path) -> None:
     mocker.patch.object(Path, "is_dir", MagicMock(return_value=True))
     mocker.patch.object(Path, "exists", MagicMock(return_value=False))
     copymock = mocker.patch("shutil.copy", MagicMock())
 
-    copy_sample_files(Path("/tmp/bar"))
+    copy_sample_files(Path(f"{tmp_path}/bar"))
     assert copymock.call_count == 3
-    assert copymock.call_args_list[0][0][1] == str(
-        Path("/tmp/bar") / "strategies/sample_strategy.py"
-    )
+    assert copymock.call_args_list[0][0][1] == str(f"{tmp_path}/bar/strategies/sample_strategy.py")
     assert copymock.call_args_list[1][0][1] == str(
-        Path("/tmp/bar") / "hyperopts/sample_hyperopt_loss.py"
+        f"{tmp_path}/bar/hyperopts/sample_hyperopt_loss.py"
     )
     assert copymock.call_args_list[2][0][1] == str(
-        Path("/tmp/bar") / "notebooks/strategy_analysis_example.ipynb"
+        f"{tmp_path}/bar/notebooks/strategy_analysis_example.ipynb"
     )
 
 
