d8eb6e59fa3cef8d7a4f2fb0b5a05781885aa2e5
Matthias <xmatthias@outlook.com>
2024-07-03 12:01:22 +0200

    Fix missing import

--- freqtrade/data/__init__.py
+++ freqtrade/data/__init__.py
@@ -2,5 +2,8 @@
 Module to handle data operations for freqtrade
 """
 
+from freqtrade.data import converter
+
+
 # limit what's imported when using `from freqtrade.data import *`
 __all__ = ["converter"]
