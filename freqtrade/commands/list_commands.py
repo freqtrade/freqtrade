bfe18573f39461c2418cc5203e290affa1270632
Matthias <xmatthias@outlook.com>
2024-07-06 10:52:03 +0200

    Improve wording for list-exchanges command

--- freqtrade/commands/list_commands.py
+++ freqtrade/commands/list_commands.py
@@ -56,11 +56,11 @@ def start_list_exchanges(args: Dict[str, Any]) -> None:
             return result
 
         if args["list_exchanges_all"]:
-            print("All exchanges supported by the ccxt library:")
             exchanges = [build_entry(e, True) for e in exchanges]
+            print(f"All exchanges supported by the ccxt library ({len(exchanges)} exchanges):")
         else:
-            print("Exchanges available for Freqtrade:")
             exchanges = [build_entry(e, False) for e in exchanges if e["valid"] is not False]
+            print(f"Exchanges available for Freqtrade ({len(exchanges)} exchanges):")
 
         print(
             tabulate(
