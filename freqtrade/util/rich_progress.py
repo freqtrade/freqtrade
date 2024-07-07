befefd449c5dbb93e39a6dacbbe78cc6f876202f
Matthias <xmatthias@outlook.com>
2024-07-07 16:13:55 +0200

    Add a generic Progressbar which allows printing additional objects

---
+++ freqtrade/util/rich_progress.py
@@ -0,0 +1,12 @@
+from rich.console import ConsoleRenderable, Group, RichCast
+from rich.progress import Progress
+
+
+class CustomProgress(Progress):
+    def __init__(self, *args, cust_objs, **kwargs) -> None:
+        self._cust_objs = cust_objs
+        super().__init__(*args, **kwargs)
+
+    def get_renderable(self) -> ConsoleRenderable | RichCast | str:
+        renderable = Group(*self._cust_objs, *self.get_renderables())
+        return renderable
