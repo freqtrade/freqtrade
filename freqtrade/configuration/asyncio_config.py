ed59f74cb8437eacee9c779d9a25fcba3d193439
Matthias <xmatthias@outlook.com>
2024-08-09 06:45:08 +0200

    chore: move asyncio import to only import when necessary

--- freqtrade/configuration/asyncio_config.py
+++ freqtrade/configuration/asyncio_config.py
@@ -1,4 +1,3 @@
-import asyncio
 import sys
 
 
@@ -6,4 +5,6 @@ def asyncio_setup() -> None:  # pragma: no cover
     # Set eventloop for win32 setups
 
     if sys.platform == "win32":
+        import asyncio
+
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
