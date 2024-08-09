2b4438720c2b0491d7563245fac4b818f40ff861
Matthias <xmatthias@outlook.com>
2024-08-09 06:16:44 +0200

    chore: call selectorPolicy

--- freqtrade/configuration/asyncio_config.py
+++ freqtrade/configuration/asyncio_config.py
@@ -2,8 +2,8 @@ import asyncio
 import sys
 
 
-def asyncio_setup() -> None:
+def asyncio_setup() -> None:  # pragma: no cover
     # Set eventloop for win32 setups
 
     if sys.platform == "win32":
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy)
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
