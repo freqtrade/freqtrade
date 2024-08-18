768b4e5e2bf89a701b0632eedf5cb2f2eaae940c
Matthias <xmatthias@outlook.com>
2024-08-18 08:38:11 +0200

    chore: Update formatting of default export sequence

--- freqtrade/strategy/__init__.py
+++ freqtrade/strategy/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa: F401
-
 from freqtrade.exchange import (
     timeframe_to_minutes,
     timeframe_to_msecs,
@@ -26,20 +25,21 @@ from freqtrade.strategy.strategy_helper import (
 
 # Imports to be used for `from freqtrade.strategy import *`
 __all__ = [
-    "timeframe_to_minutes",
-    "timeframe_to_next_date",
-    "timeframe_to_prev_date",
-    "informative",
     "IStrategy",
     "Trade",
     "Order",
     "PairLocks",
+    "informative",
     # Parameters
     "BooleanParameter",
     "CategoricalParameter",
     "DecimalParameter",
     "IntParameter",
     "RealParameter",
+    # timeframe helpers
+    "timeframe_to_minutes",
+    "timeframe_to_next_date",
+    "timeframe_to_prev_date",
     # Strategy helper functions
     "merge_informative_pair",
     "stoploss_from_absolute",
