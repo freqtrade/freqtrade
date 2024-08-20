96bc550eb916184e8ae95239c4151ab3fe68b904
Matthias <xmatthias@outlook.com>
2024-08-20 19:05:46 +0100

    Deployed 8364fc1bd to 2023.4 in en with MkDocs 1.4.2 and mike 2.1.3

---
+++ en/2023.4/javascripts/config.js
@@ -0,0 +1,12 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
