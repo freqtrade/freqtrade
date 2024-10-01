0cd051cee2906fc6336433ced1b411a22271597d
Matthias <xmatthias@outlook.com>
2024-10-01 06:44:49 +0200

    chore: update conditional check for python version

--- freqtrade/__main__.py
+++ freqtrade/__main__.py
@@ -3,7 +3,7 @@
 __main__.py for Freqtrade
 To launch Freqtrade as a module
 
-> python -m freqtrade (with Python >= 3.9)
+> python -m freqtrade (with Python >= 3.10)
 """
 
 from freqtrade import main
