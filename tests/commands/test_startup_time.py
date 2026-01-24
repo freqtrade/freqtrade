c38dd404758e6a8cedcc880caf03acbf8543fe85
vijay sharma <vijaychandrika06@gmail.com>
2026-01-24 06:32:31 +0100

    jhkj

--- tests/commands/test_startup_time.py
+++ tests/commands/test_startup_time.py
@@ -1,4 +1,5 @@
-import subprocess  # noqa: S404, RUF100
+import subprocess
+import sys
 import time
 
 from tests.conftest import is_arm, is_mac
@@ -8,11 +9,13 @@ MAXIMUM_STARTUP_TIME = 0.7 if is_mac() and not is_arm(True) else 0.5
 
 
 def test_startup_time():
+    # command to run
+    cmd = [sys.executable, "-m", "freqtrade", "-h"]
     # warm up to generate pyc
-    subprocess.run(["freqtrade", "-h"])
+    subprocess.run(cmd)
 
     start = time.time()
-    subprocess.run(["freqtrade", "-h"])
+    subprocess.run(cmd)
     elapsed = time.time() - start
     assert elapsed < MAXIMUM_STARTUP_TIME, (
         "The startup time is too long, try to use lazy import in the command entry function"
