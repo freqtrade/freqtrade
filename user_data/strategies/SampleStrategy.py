84bec4f1d11977371f1c1d1d5b8b5eb944135cef
Preetham Akhil Bhuma <preethambhuma17@gmail.com>
2026-03-22 02:00:15 -0700

    edit stoploss to 4%

--- user_data/strategies/SampleStrategy.py
+++ user_data/strategies/SampleStrategy.py
@@ -73,7 +73,7 @@ class SampleStrategy(IStrategy):
 
     # Optimal stoploss designed for the strategy.
     # This attribute will be overridden if the config file contains "stoploss".
-    stoploss = -0.10
+    stoploss = -0.04
 
     # Trailing stoploss
     trailing_stop = False
