f5a0b1d90f25b2b1db3d9d8be26a771c3f6a9d50
ChinhLee <76194645+chinhkrb113@users.noreply.github.com>
2026-03-29 00:38:50 +0700

    feat: add trailing stop order type enum
    Add a new order type value for exchange-native trailing stops to distinguish them from regular stop-loss orders. This will allow the system to differentiate between Freqtrade-managed trailing stops and exchange-native trailing stops.
    
    Affected files: ordertypevalue.py, binance.py
    
    Signed-off-by: ChinhLee <76194645+chinhkrb113@users.noreply.github.com>

--- freqtrade/enums/ordertypevalue.py
+++ freqtrade/enums/ordertypevalue.py
@@ -4,3 +4,5 @@ from enum import StrEnum
 class OrderTypeValues(StrEnum):
     limit = "limit"
     market = "market"
+    trailing_stop_market = "trailing_stop_market"
+    trailing_stop = "trailing_stop"
