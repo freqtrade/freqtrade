6c6d0d4f604e9f062ee925a0d6888e62096a4a8f
Stefano <stefano.ariestasia@gmail.com>
2026-07-12 15:05:01 +0900

    fix weird ruff error

--- freqtrade/freqai/data_kitchen.py
+++ freqtrade/freqai/data_kitchen.py
@@ -1,8 +1,8 @@
-import copy
 import inspect
 import logging
 import random
 import shutil
+from copy import deepcopy
 from datetime import UTC, datetime
 from pathlib import Path
 from typing import Any
@@ -596,7 +596,7 @@ class FreqaiDataKitchen:
         self.model_filename = f"cb_{coin.lower()}_{timestamp_id}"
 
     def set_all_pairs(self) -> None:
-        self.all_pairs = copy.deepcopy(
+        self.all_pairs = deepcopy(
             self.freqai_config["feature_parameters"].get("include_corr_pairlist", [])
         )
         for pair in self.config.get("exchange", "").get("pair_whitelist"):
