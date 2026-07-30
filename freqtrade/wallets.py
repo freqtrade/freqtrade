02ff7c4a2c63308df8fa7221e3a52bce3d286ef9
Matthias <xmatthias@outlook.com>
2026-07-30 07:21:06 +0200

    fix: don't persist 0 balances to wallet history

--- freqtrade/wallets.py
+++ freqtrade/wallets.py
@@ -486,7 +486,8 @@ class Wallets:
             wallet_records.append(position_record)
 
         for wallet in self.get_all_balances().values():
-            # TODO: (needs decision) exclude minimal balances?
+            if wallet.total == 0:
+                continue
             rate = self._exchange.get_conversion_rate(wallet.currency, self._stake_currency)
             is_bot_managed = (
                 self._stake_currency == wallet.currency or wallet.currency in open_assets
