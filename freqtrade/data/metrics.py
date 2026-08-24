fix: avoid fragmented dataframe, fix mean calculation problem
closes #13481

freqtrade/data/metrics.py
@@ -75,11 +75,13 @@ def combined_dataframes_with_rel_mean(
     df_comb = combine_dataframes_by_column(data, column)
     # Trim dataframes to the given timeframe
     df_comb = df_comb.iloc[(df_comb.index >= fromdt) & (df_comb.index < todt)]
-    rel_mean = df_comb.pct_change().mean(axis=1).fillna(0).cumsum()
-    df_comb["count"] = df_comb.count(axis=1)
-    df_comb["mean"] = df_comb.mean(axis=1)
-    df_comb["rel_mean"] = rel_mean
-    return df_comb[["mean", "rel_mean", "count"]]
+    return pd.DataFrame(
+        {
+            "mean": df_comb.mean(axis=1),
+            "rel_mean": df_comb.pct_change().mean(axis=1).fillna(0).cumsum(),
+            "count": df_comb.count(axis=1),
+        }
+    )
 
 
 def combine_dataframes_with_mean(
@@ -95,9 +97,7 @@ def combine_dataframes_with_mean(
     """
     df_comb = combine_dataframes_by_column(data, column)
 
-    df_comb["mean"] = df_comb.mean(axis=1)
-
-    return df_comb
+    return pd.concat([df_comb, df_comb.mean(axis=1).rename("mean")], axis=1)
 
 
 def create_cum_profit(