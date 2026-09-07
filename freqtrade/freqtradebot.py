6f2d9f2197891af8e327147e3738c4d0b8cc8d98
Matthias <xmatthias@outlook.com>
2026-09-07 07:11:35 +0200

    fix: consider existing trade-amount for entry cancels
    closes #13537

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -1922,11 +1922,11 @@ class FreqtradeBot(LoggingMixin):
             filled_val: float = order.get("filled", 0.0) or 0.0
 
             if filled_val > 0:
-                filled_stake = filled_val * trade.open_rate
+                remaining_stake = (trade.amount + filled_val) * trade.open_rate
                 minstake = self.exchange.get_min_pair_stake_amount(
                     trade.pair, trade.open_rate, self.strategy.stoploss
                 )
-                if minstake and filled_stake < minstake:
+                if minstake and remaining_stake < minstake:
                     logger.warning(
                         f"Order {order_id} for {trade.pair} not cancelled, "
                         f"as the filled amount of {filled_val} would result in an unexitable trade."
